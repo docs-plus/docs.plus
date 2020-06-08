24ffab24a71aa8417632c3c8627d9bd30f764719
Samir Sayyad <samir.saiad@gmail.com>
2020-06-08 16:46:32 +0430

    change title skin

--- src/static/skins/colibris/pad.js
+++ src/static/skins/colibris/pad.js
@@ -1,6 +1,7 @@
 function customStart()
 {
   $('#pad_title').show();
+  $('#pad_title').prepend('<img class="logo" src="../static/images/logo.png?v=<%=settings.randomVersionString%>">');
   $('.buttonicon').mousedown(function() { $(this).parent().addClass('pressed'); })
   $('.buttonicon').mouseup(function() { $(this).parent().removeClass('pressed'); })
 }
