4a6950ddd8390f7603b93e158eeca595a92049ac
hmarzban <marzban98@gmail.com>
2020-07-03 11:46:48 +0430

    feat: support domain.com/*

--- src/node/hooks/express/specialpages.js
+++ src/node/hooks/express/specialpages.js
@@ -11,6 +11,41 @@ exports.expressCreateServer = function (hook_name, args, cb) {
     res.json(require('ep_etherpad-lite/node/stats').toJSON())
   })
 
+  //serve pad.html under /p
+  args.app.get(['/:pad', '/p/:pad'], async function(req, res, next)
+  {
+    // The below might break for pads being rewritten
+    var isReadOnly = req.url.indexOf("/p/r.") === 0;
+
+    hooks.callAll("padInitToolbar", {
+      toolbar: toolbar,
+      isReadOnly: isReadOnly
+    });
+    // @Samir Sayyad Added for social preview
+    let pad_title = await db.get("title:"+req.params.pad) ;
+    //console.log("Found ", pad_title, " for ", req.params.pad);
+  
+    res.send(eejs.require("ep_etherpad-lite/templates/pad.html", {
+      meta : { title : (pad_title) ? pad_title :req.params.pad } ,
+      req: req,
+      toolbar: toolbar,
+      isReadOnly: isReadOnly
+    }));
+  });
+
+  //serve timeslider.html under /p/$padname/timeslider
+  args.app.get(['/:pad/timeslider', '/p/:pad/timeslider'], function(req, res, next)
+  {
+    hooks.callAll("padInitToolbar", {
+      toolbar: toolbar
+    });
+
+    res.send(eejs.require("ep_etherpad-lite/templates/timeslider.html", {
+      req: req,
+      toolbar: toolbar
+    }));
+  });
+
   //serve index.html under /
   args.app.get('/', function(req, res)
   {
@@ -22,7 +57,6 @@ exports.expressCreateServer = function (hook_name, args, cb) {
   {
     res.send(eejs.require("ep_etherpad-lite/templates/javascript.html"));
   });
-
 
   //serve robots.txt
   args.app.get('/robots.txt', function(req, res)
@@ -37,41 +71,6 @@ exports.expressCreateServer = function (hook_name, args, cb) {
         res.sendFile(filePath);
       }
     });
-  });
-
-  //serve pad.html under /p
-  args.app.get('/p/:pad', async function(req, res, next)
-  {
-    // The below might break for pads being rewritten
-    var isReadOnly = req.url.indexOf("/p/r.") === 0;
-
-    hooks.callAll("padInitToolbar", {
-      toolbar: toolbar,
-      isReadOnly: isReadOnly
-    });
-    // @Samir Sayyad Added for social preview
-    let pad_title = await db.get("title:"+req.params.pad) ;
-    //console.log("Found ", pad_title, " for ", req.params.pad);
- 
-    res.send(eejs.require("ep_etherpad-lite/templates/pad.html", {
-      meta : { title : (pad_title) ? pad_title :req.params.pad } ,
-      req: req,
-      toolbar: toolbar,
-      isReadOnly: isReadOnly
-    }));
-  });
-
-  //serve timeslider.html under /p/$padname/timeslider
-  args.app.get('/p/:pad/timeslider', function(req, res, next)
-  {
-    hooks.callAll("padInitToolbar", {
-      toolbar: toolbar
-    });
-
-    res.send(eejs.require("ep_etherpad-lite/templates/timeslider.html", {
-      req: req,
-      toolbar: toolbar
-    }));
   });
 
   //serve favicon.ico from all path levels except as a pad name
