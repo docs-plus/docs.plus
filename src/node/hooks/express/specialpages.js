a4364d9ca371f22b6c6a284c4a6011d2c38884b1
Hossein M <marzban98@gmail.com>
2020-09-23 12:42:38 +0330

    fix: router make more uglier :(

--- src/node/hooks/express/specialpages.js
+++ src/node/hooks/express/specialpages.js
@@ -18,11 +18,55 @@ exports.expressCreateServer = function (hook_name, args, cb) {
     res.send(eejs.require("ep_etherpad-lite/templates/index.html"));
   });
 
+    //  serve pad.html under /p
+    args.app.get(/^\/(?!.*(static|javascripts|javascript|pluginfw|socket.io|tests|lib))(.*)+/i, async function(req, res, next){
+      // The below might break for pads being rewritten
+      console.log(req.params, req.url)
+      const isReadOnly = req.url.indexOf("/p/r.") === 0;
+      const padId = req.params['1'].split('/').join(":");
+      const staticRootAddress = req.path.split("/")
+        .filter(x=> x.length)
+        .map(path => "../")
+        .join("");
+      hooks.callAll("padInitToolbar", {
+        toolbar: toolbar,
+        isReadOnly: isReadOnly
+      });
+       // @Samir Sayyad Added for social preview
+       let pad_title = await db.get("title:"+req.params.pad) ;
+       //console.log("Found ", pad_title, " for ", req.params.pad);
+      res.send(eejs.require("ep_etherpad-lite/templates/pad.html", {
+         meta : { title : (pad_title) ? pad_title :req.params.pad } ,
+         padId,
+         staticRootAddress,
+         req: req,
+         toolbar: toolbar,
+         isReadOnly: isReadOnly
+      }));
+    });
+
   //serve javascript.html
   args.app.get('/javascript', function(req, res)
   {
     res.send(eejs.require("ep_etherpad-lite/templates/javascript.html"));
   });
+
+  //serve favicon.ico from all path levels except as a pad name
+  args.app.get(/static\/favicon.ico$/, function(req, res)
+  {
+    var filePath = path.join(settings.root, "src", "static", "skins", settings.skinName, "favicon.ico");
+
+    res.sendFile(filePath, function(err)
+    {
+      //there is no custom favicon, send the default favicon
+      if(err)
+      {
+        filePath = path.join(settings.root, "src", "static", "favicon.ico");
+        res.sendFile(filePath);
+      }
+    });
+  });
+  
 
   // Backward compatibility for plugins
   args.app.get(/(\/static\/plugins\/(.*))/ , function (req, res, next) {
@@ -34,7 +78,7 @@ exports.expressCreateServer = function (hook_name, args, cb) {
   });
 
   //serve timeslider.html under /p/$padname/timeslider
-  args.app.get(/^\/p\/(.*)\/timeslider$/i, function(req, res, next){
+  args.app.get(/^\/(.*)\/timeslider$/i, function(req, res, next){
       const padId = req.params['0'].split('/').join(":");
       const staticRootAddress = req.path.split("/")
         .filter(x=> x.length)
@@ -51,33 +95,8 @@ exports.expressCreateServer = function (hook_name, args, cb) {
       }));
     });
 
-   //serve pad.html under /p
-   args.app.get(/^\/p\/(.*)/i, function(req, res, next){
-     // The below might break for pads being rewritten
-     const isReadOnly = req.url.indexOf("/p/r.") === 0;
-     const padId = req.params['0'].split('/').join(":");
-     const staticRootAddress = req.path.split("/")
-       .filter(x=> x.length)
-       .map(path => "../")
-       .join("");
-     hooks.callAll("padInitToolbar", {
-       toolbar: toolbar,
-       isReadOnly: isReadOnly
-     });
-      // @Samir Sayyad Added for social preview
-      let pad_title = await db.get("title:"+req.params.pad) ;
-      //console.log("Found ", pad_title, " for ", req.params.pad);
-     res.send(eejs.require("ep_etherpad-lite/templates/pad.html", {
-        meta : { title : (pad_title) ? pad_title :req.params.pad } ,
-        padId,
-        staticRootAddress,
-        req: req,
-        toolbar: toolbar,
-        isReadOnly: isReadOnly
-     }));
-   });
 
-  //serve robots.txt
+      //serve robots.txt
   args.app.get('/robots.txt', function(req, res)
   {
     var filePath = path.join(settings.root, "src", "static", "skins", settings.skinName, "robots.txt");
@@ -92,22 +111,6 @@ exports.expressCreateServer = function (hook_name, args, cb) {
     });
   });
 
-
-  //serve favicon.ico from all path levels except as a pad name
-  args.app.get( /static\/favicon.ico$/, function(req, res)
-  {
-    var filePath = path.join(settings.root, "src", "static", "skins", settings.skinName, "favicon.ico");
-
-    res.sendFile(filePath, function(err)
-    {
-      //there is no custom favicon, send the default favicon
-      if(err)
-      {
-        filePath = path.join(settings.root, "src", "static", "favicon.ico");
-        res.sendFile(filePath);
-      }
-    });
-  });
 
 
 }
