793ca9de830f0b830a44905de06c2a52afd1b249
Samir Sayyad <samir.saiad@gmail.com>
2020-11-19 13:47:01 +0330

    add ecosystem hand have just one runner

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,16 +17,23 @@ module.exports = {
         ACCESS_KEY_SECRET : `${process.env.ACCESS_KEY_SECRET}`,
         MEDIA_BUCKET_NAME : `${process.env.MEDIA_BUCKET_NAME}`,
       },
-      instances: 2,
-
     },
     {
-      name: "docs_plus_dev",
+      name: "handbook",
       script: "./node_modules/ep_etherpad-lite/node/server.js",
       env: {
-        NODE_ENV: "development",
-        PORT:"9002",
-
+        NODE_ENV: "production",
+        PORT:`${process.env.PORT}`,
+        DB_USER : `${process.env.DB_USER}`,
+        DB_PASS : `${process.env.DB_PASS}`,
+        DB_HOST : `${process.env.DB_HOST}`,
+        DB_TYPE : `${process.env.DB_TYPE}`,
+        DB_PORT : `${process.env.DB_PORT}`,
+        DB_NAME : `${process.env.DB_NAME}`,
+        BUCKET_S3_SERVER : `${process.env.BUCKET_S3_SERVER}`,
+        ACCESS_KEY_ID : `${process.env.ACCESS_KEY_ID}`,
+        ACCESS_KEY_SECRET : `${process.env.ACCESS_KEY_SECRET}`,
+        MEDIA_BUCKET_NAME : `${process.env.MEDIA_BUCKET_NAME}`,
       },
     }
 
