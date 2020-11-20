32c01f737e94d5faa270580fc5a6b5fa76942382
Samir Sayyad <samir.saiad@gmail.com>
2020-11-20 16:39:56 +0330

    add this branch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -55,6 +55,24 @@ module.exports = {
       },
     },
     {
+      name: "dev-samir",
+      script: "./node_modules/ep_etherpad-lite/node/server.js",
+      env: {
+        NODE_ENV: "production",
+        PORT:"9004",
+        DB_USER : `${process.env.DB_USER}`,
+        DB_PASS : `${process.env.DB_PASS}`,
+        DB_HOST : `${process.env.DB_HOST}`,
+        DB_TYPE : `${process.env.DB_TYPE}`,
+        DB_PORT : `${process.env.DB_PORT}`,
+        DB_NAME : `${process.env.DB_NAME}`,
+        BUCKET_S3_SERVER : `${process.env.BUCKET_S3_SERVER}`,
+        ACCESS_KEY_ID : `${process.env.ACCESS_KEY_ID}`,
+        ACCESS_KEY_SECRET : `${process.env.ACCESS_KEY_SECRET}`,
+        MEDIA_BUCKET_NAME : `${process.env.MEDIA_BUCKET_NAME}`,
+      },
+    },
+    {
       name: "handbook",
       script: "./node_modules/ep_etherpad-lite/node/server.js",
       env: {
