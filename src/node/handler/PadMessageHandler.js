6727b2b4eaf456febd1e9819101afabe5fff007e
Hossein M <marzban98@gmail.com>
2021-01-08 18:44:37 +0330

    merge chat.js and index.html

--- src/node/handler/PadMessageHandler.js
+++ src/node/handler/PadMessageHandler.js
@@ -344,8 +344,10 @@ async function handleChatMessage(socket, message) {
  * @param userId the author id of the chat message
  * @param text the text of the chat message
  * @param padId the padId to send the chat message to
+ * @param chatTextType kind of chat text message that add by @Samir
+ * @param beforeText kind of chat text message that add by @Samir
  */
-exports.sendChatMessageToPadClients = async function (time, userId, text, padId) {
+exports.sendChatMessageToPadClients = async function (time, userId, text, padId, chatTextType, beforeText) {
   // get the pad
   const pad = await padManager.getPad(padId);
 
@@ -353,11 +355,11 @@ exports.sendChatMessageToPadClients = async function (time, userId, text, padId)
   const userName = await authorManager.getAuthorName(userId);
 
   // save the chat message
-  const promise = pad.appendChatMessage(text, userId, time);
+  const promise = pad.appendChatMessage(text, userId, time, chatTextType, beforeText);
 
   const msg = {
     type: 'COLLABROOM',
-    data: {type: 'CHAT_MESSAGE', userId, userName, time, text},
+    data: {type: 'CHAT_MESSAGE', userId, userName, time, text, chatTextType, beforeText},
   };
 
   // broadcast the chat message to everyone on the pad
