e8d71167fe61604e37074121482b4714cc9365f9
Samir Sayyad <samir.saiad@gmail.com>
2021-01-11 21:08:07 +0330

    add tets branch just for testing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -92,7 +92,25 @@ module.exports = {
       },
     },
     
-    
+    {
+      name: "test",
+      script: "./node_modules/ep_etherpad-lite/node/server.js",
+      env: {
+        NODE_ENV: "production",
+        PORT:"9006",
+        DB_USER : `${process.env.DB_USER}`,
+        DB_PASS : `${process.env.DB_PASS}`,
+        DB_HOST : `${process.env.DB_HOST}`,
+        DB_TYPE : `${process.env.DB_TYPE}`,
+        DB_PORT : `${process.env.DB_PORT}`,
+        DB_NAME : `${process.env.DB_NAME}`,
+        SSL : `${process.env.SSL}`,
+        BUCKET_S3_SERVER : `${process.env.BUCKET_S3_SERVER}`,
+        ACCESS_KEY_ID : `${process.env.ACCESS_KEY_ID}`,
+        ACCESS_KEY_SECRET : `${process.env.ACCESS_KEY_SECRET}`,
+        MEDIA_BUCKET_NAME : `${process.env.MEDIA_BUCKET_NAME}`,
+      },
+    },
     
     
 
