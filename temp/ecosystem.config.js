8afff889383bef2bf73fe6d70918a4b81caaf2a5
Samir Sayyad <samir.saiad@gmail.com>
2021-04-13 10:06:09 +0430

    add vanilla

--- temp/ecosystem.config.js
+++ temp/ecosystem.config.js
@@ -111,6 +111,25 @@ module.exports = {
       MEDIA_BUCKET_NAME : `${process.env.MEDIA_BUCKET_NAME}`,
     },
   },
+  {
+    name: "vanilla",
+    script: "./node_modules/ep_etherpad-lite/node/server.js",
+    env: {
+      NODE_ENV: "production",
+      PORT:"9007",
+      DB_USER : `${process.env.DB_USER}`,
+      DB_PASS : `${process.env.DB_PASS}`,
+      DB_HOST : `${process.env.DB_HOST}`,
+      DB_TYPE : `${process.env.DB_TYPE}`,
+      DB_PORT : `${process.env.DB_PORT}`,
+      DB_NAME : `${process.env.DB_NAME}`,
+      SSL : `${process.env.SSL}`,
+      BUCKET_S3_SERVER : `${process.env.BUCKET_S3_SERVER}`,
+      ACCESS_KEY_ID : `${process.env.ACCESS_KEY_ID}`,
+      ACCESS_KEY_SECRET : `${process.env.ACCESS_KEY_SECRET}`,
+      MEDIA_BUCKET_NAME : `${process.env.MEDIA_BUCKET_NAME}`,
+    },
+  },
   
   
 
