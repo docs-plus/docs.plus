95f5f2a18c0fcbed533840bd5755f294e8711123
Samir Sayyad <samir.saiad@gmail.com>
2021-08-03 16:39:40 +0430

    democracy <> rocketchat

--- temp/ecosystem.config.js
+++ temp/ecosystem.config.js
@@ -133,6 +133,28 @@ module.exports = {
       MEDIA_BUCKET_NAME : `${process.env.MEDIA_BUCKET_NAME}`,
     },
   },
+  {
+    name: "democracy",
+    script: "./node_modules/ep_etherpad-lite/node/server.js",
+    env: {
+      NODE_ENV: "production",
+      PORT:"9101",
+      DB_USER : `${process.env.DB_USER}`,
+      DB_PASS : `${process.env.DB_PASS}`,
+      DB_HOST : `${process.env.DB_HOST}`,
+      DB_TYPE : `${process.env.DB_TYPE}`,
+      DB_PORT : `${process.env.DB_PORT}`,
+      DB_NAME : `${process.env.DB_NAME}`,
+      SSL : `${process.env.SSL}`,
+      BUCKET_S3_SERVER : `${process.env.BUCKET_S3_SERVER}`,
+      ACCESS_KEY_ID : `${process.env.ACCESS_KEY_ID}`,
+      ACCESS_KEY_SECRET : `${process.env.ACCESS_KEY_SECRET}`,
+      MEDIA_BUCKET_NAME : `${process.env.MEDIA_BUCKET_NAME}`,
+      ROCKET_CHAT_ADMIN_USERID : `${process.env.ROCKET_CHAT_ADMIN_USERID_DEMOCRACY}`,
+      ROCKET_CHAT_ADMIN_TOKEN : `${process.env.ROCKET_CHAT_ADMIN_TOKEN_DEMOCRACY}`,
+
+    },
+  },
   
   
 
