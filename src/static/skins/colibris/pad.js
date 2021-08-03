82a7d434e6649a75599beaebfa7179477043c9a4
Hossein M <marzban98@gmail.com>
2021-08-03 17:43:29 +0430

    fix: logo address

--- src/static/skins/colibris/pad.js
+++ src/static/skins/colibris/pad.js
@@ -3,7 +3,7 @@
 window.customStart = () => {
   const staticRoot = clientVars.staticRootAddress
   $('#pad_title').show();
-  $('#pad_title').prepend(`<a href="${staticRoot}"><img class="logo" src="${staticRoot}/static/images/logo.png"> </a>`);
+  $('#pad_title').prepend(`<a href="${staticRoot}"><img class="logo" src="${staticRoot}static/images/logo.png"> </a>`);
   $('.buttonicon').mousedown(function () { $(this).parent().addClass('pressed'); });
   $('.buttonicon').mouseup(function () { $(this).parent().removeClass('pressed'); });
 };
