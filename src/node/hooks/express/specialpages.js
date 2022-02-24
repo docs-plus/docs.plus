749d79956d63e88480e2fd095560bdf04776ef91
hossein <marzban98@gmail.com>
2022-02-24 10:29:00 +0330

    lint

--- src/node/hooks/express/specialpages.js
+++ src/node/hooks/express/specialpages.js
@@ -10,7 +10,7 @@ const settings = require('../../utils/Settings');
 const util = require('util');
 const webaccess = require('./webaccess');
 const padInfo = require('../../utils/nestedPad'); // @Hossein
-const db = require("../../db/DB");  // @Samir
+const db = require('../../db/DB'); // @Samir
 const minify = require('../../utils/Minify'); // @Hossein
 
 exports.expressCreateServer = (hookName, args, cb) => {
@@ -20,10 +20,10 @@ exports.expressCreateServer = (hookName, args, cb) => {
   });
 
   // @Hossein
-  args.app.get(/(\/static\/plugins\/(.*))/ , (req, res, next) => {
-    const path = req.path.split("/");
-    const startPath = path.findIndex(path => path === "plugins");
-    const newPath = path.slice(startPath, path.length).join("/");
+  args.app.get(/(\/static\/plugins\/(.*))/, (req, res, next) => {
+    const path = req.path.split('/');
+    const startPath = path.findIndex((path) => path === 'plugins');
+    const newPath = path.slice(startPath, path.length).join('/');
     req.params.filename = newPath;
     return minify.minify(req, res);
   });
@@ -62,18 +62,18 @@ exports.expressCreateServer = (hookName, args, cb) => {
   // serve timeslider.html under /p/$padname/timeslider
   args.app.get('/p/:pad*/timeslider', async (req, res, next) => {
     const {padId, padName, padView} = padInfo(req);
-    const staticRootAddress = req.path.split("/")
-    .filter(x=> x.length)
-    .map(path => "../")
-    .join("");
+    const staticRootAddress = req.path.split('/')
+        .filter((x) => x.length)
+        .map((path) => '../')
+        .join('');
 
     hooks.callAll('padInitToolbar', {
       toolbar,
     });
     // @Samir Sayyad Added for social preview
-    const pad_title = await db.get("title:"+ padId);
+    const padTitle = await db.get(`title:${padId}`);
     res.send(eejs.require('ep_etherpad-lite/templates/timeslider.html', {
-      meta : { title : (pad_title) ? pad_title : req.params.pad },
+      meta: {title: (padTitle) ? padTitle : req.params.pad},
       req,
       toolbar,
       padId,
@@ -85,15 +85,15 @@ exports.expressCreateServer = (hookName, args, cb) => {
   // @Hossein
   // serve pad.html under /p
   args.app.get('/p/:pad*', async (req, res, next) => {
-    let staticRootAddress = req.path.split("/");
+    let staticRootAddress = req.path.split('/');
     // The below might break for pads being rewritten
     const isReadOnly = !webaccess.userCanModify(req.params.pad, req);
-    let {padId, padName, padView} = padInfo(req, isReadOnly);
+    const {padId, padName, padView} = padInfo(req, isReadOnly);
 
-    staticRootAddress = req.path.split("/")
-      .filter(x=> x.length)
-      .map(path => "../")
-      .join("");
+    staticRootAddress = req.path.split('/')
+        .filter((x) => x.length)
+        .map((path) => '../')
+        .join('');
 
     hooks.callAll('padInitToolbar', {
       toolbar,
@@ -104,12 +104,12 @@ exports.expressCreateServer = (hookName, args, cb) => {
     // can be removed when require-kernel is dropped
     res.header('Feature-Policy', 'sync-xhr \'self\'');
     // @Samir Sayyad Added for social preview
-    const pad_title = await db.get("title:"+ padId);
+    const padTitle = await db.get(`title:${padId}`);
 
     // can be removed when require-kernel is dropped
     res.header('Feature-Policy', 'sync-xhr \'self\'');
     res.send(eejs.require('ep_etherpad-lite/templates/pad.html', {
-      meta : { title : (pad_title) ? pad_title : req.params.pad },
+      meta: {title: (padTitle) ? padTitle : req.params.pad},
       padId,
       padView,
       padName,
@@ -120,25 +120,24 @@ exports.expressCreateServer = (hookName, args, cb) => {
     }));
   });
 
-  args.app.get(/\/favicon.ico$/, (req, res, next) =>
-    (async () => {
-      const fns = [
-        ...(settings.favicon ? [path.resolve(settings.root, settings.favicon)] : []),
-        path.join(settings.root, 'src', 'static', 'skins', settings.skinName, 'favicon.ico'),
-        path.join(settings.root, 'src', 'static', 'favicon.ico'),
-      ];
-      for (const fn of fns) {
-        try {
-          await fsp.access(fn, fs.constants.R_OK);
-        } catch (err) {
-          continue;
-        }
-        res.setHeader('Cache-Control', `public, max-age=${settings.maxAge}`);
-        await util.promisify(res.sendFile.bind(res))(fn);
-        return;
+  args.app.get(/\/favicon.ico$/, (req, res, next) => (async () => {
+    const fns = [
+      ...(settings.favicon ? [path.resolve(settings.root, settings.favicon)] : []),
+      path.join(settings.root, 'src', 'static', 'skins', settings.skinName, 'favicon.ico'),
+      path.join(settings.root, 'src', 'static', 'favicon.ico'),
+    ];
+    for (const fn of fns) {
+      try {
+        await fsp.access(fn, fs.constants.R_OK);
+      } catch (err) {
+        continue;
       }
-      next();
-    })().catch((err) => next(err || new Error(err))));
+      res.setHeader('Cache-Control', `public, max-age=${settings.maxAge}`);
+      await util.promisify(res.sendFile.bind(res))(fn);
+      return;
+    }
+    next();
+  })().catch((err) => next(err || new Error(err))));
 
 
   return cb();
