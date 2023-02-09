3e1963ed116cd6ac6abdd93f4a08f96af98ac9e4
Hossein Marzban <marzban98@gmail.com>
2023-02-09 12:43:44 +0330

    fix: clean

--- packages/web/src/components/TipTap/extentions/changeHeading2paragraphs.js
+++ packages/web/src/components/TipTap/extentions/changeHeading2paragraphs.js
@@ -1,24 +1,10 @@
-import { getSelectionBlocks, getRangeBlocks, getPrevHeadingList } from './helper'
 import onHeading from './normalText/onHeading'
 import onSelection from './normalText/onSelection'
+
 export default (arrg) => {
-  const { can, chain, commands, dispatch, editor, state, tr, view } = arrg
-  const { schema, selection, doc } = state
-  const { $from, $to, $anchor, $cursor, $head } = selection
-  const { start, end, depth } = $from.blockRange($to)
-
-  const selectedContents = getSelectionBlocks(doc, start, $head.pos)
-  const contentWrapper = getRangeBlocks(doc, start - 1, $from.start(1) - 1 + $from.doc.nodeAt($from.start(1) - 1).content.size)
-
-  console.log({
-    selectedContents,
-    contentWrapper,
-    $from,
-    $to,
-    $anchor,
-    $cursor,
-    $head
-  })
+  const { state } = arrg
+  const { selection } = state
+  const { $anchor, $head } = selection
 
   if ($anchor.pos === $head.pos) {
     console.info('[Heading], normalText on Heading block')
