532502eba04fc90535a2d49468f809d8f7d9ad2d
Hossein Marzban <marzban98@gmail.com>
2023-02-14 09:48:51 +0330

    refactor: refactor heading buttons #52

--- packages/web/src/components/TipTap/extentions/ContentHeading.jsx
+++ packages/web/src/components/TipTap/extentions/ContentHeading.jsx
@@ -1,20 +1,127 @@
+import { Selection, Plugin, TextSelection, PluginKey } from 'prosemirror-state'
+import { Decoration, DecorationSet } from 'prosemirror-view'
 import { Node, mergeAttributes } from '@tiptap/core'
-import { Decoration, DecorationSet, EditorView } from 'prosemirror-view'
-import { ObjectID } from 'bson'
-
-import { Plugin } from 'prosemirror-state'
 
 import joinH1ToPrevHeading from './joinH1ToPrevHeading'
+import { copyToClipboard } from './helper'
 
-function copyToClipboard (text) {
-  // const content = document.createElement("<div/>")
-  // content.innerHTML = text
-  // const copyText = document.getElementById("content").value;
-  navigator.clipboard.writeText(text).then(() => {
-    // Alert the user that the action took place.
-    // Nobody likes hidden stuff being done under the hood!
-    alert('Copied to clipboard')
+function extractContentHeadingBlocks (doc) {
+  const result = []
+  const record = (from, to, open, headingId, node) => {
+    result.push({ from, to, open, headingId, node })
+  }
+  let lastHeadingId
+
+  // For each node in the document
+  doc.descendants((node, pos) => {
+    if (node.type.name === 'heading') {
+      lastHeadingId = node.attrs.id
+    }
+    if (node.type.name === 'contentHeading') {
+      const nodeSize = node.nodeSize
+
+      record(pos, pos + nodeSize, node.attrs.open, lastHeadingId, node)
+    }
   })
+
+  return result
+}
+
+const buttonWrapper = (editor, { headingId, open, from, node }) => {
+  const buttonWrapper = document.createElement('div')
+  const btnToggleHeading = document.createElement('button')
+  const btnChatBox = document.createElement('button')
+
+  buttonWrapper.classList.add('buttonWrapper')
+
+  btnChatBox.setAttribute('class', 'btn_openChatBox')
+  btnChatBox.setAttribute('type', 'button')
+  btnToggleHeading.classList.add('btnFold')
+  btnToggleHeading.setAttribute('type', 'button')
+
+  buttonWrapper.append(btnToggleHeading)
+  buttonWrapper.append(btnChatBox)
+
+  const toggleHeadingContent = (el) => {
+    const detailsContent = document.querySelector(`.heading[data-id="${headingId}"] div.contentWrapper`)
+    const event = new CustomEvent('toggleHeadingsContent', { detail: { headingId, open, el: detailsContent } })
+
+    detailsContent === null || detailsContent === void 0 ? void 0 : detailsContent.dispatchEvent(event)
+  }
+
+  const foldAndUnfold = (e) => {
+    const el = e.target
+
+    editor.commands.focus(from + node.nodeSize - 1)
+    if (editor.isEditable) {
+      editor
+        .chain()
+        .command(({ tr }) => {
+          const pos = from
+          const currentNode = tr.doc.nodeAt(pos)
+          const headingNode = tr.doc.nodeAt(pos - 1)
+
+          // if (currentNode.type.name !== 'contentHeading') {
+          //   return false
+          // }
+
+          tr.setNodeMarkup(pos, undefined, {
+            ...currentNode.attrs,
+            open: !currentNode.attrs.open,
+            level: currentNode.attrs.level
+          }).setNodeMarkup(pos - 1, undefined, {
+            ...headingNode.attrs,
+            open: !currentNode.attrs.open,
+            level: currentNode.attrs.level
+          })
+
+          return true
+        })
+        .run()
+
+      toggleHeadingContent(el)
+    }
+  }
+
+  btnToggleHeading.addEventListener('click', foldAndUnfold)
+
+  // copy the link to clipboard
+  const href = document.createElement('a')
+
+  href.innerHTML = '#'
+  href.setAttribute('href', `#${headingId}`)
+  href.addEventListener('click', (e) => {
+    e.preventDefault()
+    const url = new URL(window.location)
+
+    url.searchParams.set('id', headingId)
+    window.history.pushState({}, '', url)
+    copyToClipboard(url)
+    editor
+      .chain()
+      .focus(from + node.nodeSize - 1)
+      .run()
+  })
+  href.contenteditable = false
+  buttonWrapper.append(href)
+
+  return buttonWrapper
+}
+
+const appendButtonsDec = (doc, editor) => {
+  const decos = []
+  const contentWrappers = extractContentHeadingBlocks(doc)
+
+  contentWrappers.forEach(prob => {
+    const decorationWidget = Decoration.widget(prob.to, buttonWrapper(editor, prob), {
+      side: -1,
+      key: prob.headingId
+    })
+
+    decos.push(decorationWidget)
+  })
+
+  return DecorationSet.create(doc, decos)
 }
 
 const HeadingsTitle = Node.create({
@@ -28,7 +135,6 @@ const HeadingsTitle = Node.create({
   allowGapCursor: false,
   addOptions () {
     return {
-      openClassName: 'is-open',
       open: true,
       HTMLAttributes: {
         class: 'title'
@@ -44,136 +150,6 @@ const HeadingsTitle = Node.create({
       level: {
         default: 1,
         rendered: false
-      }
-    }
-  },
-  addNodeView () {
-    return ({ editor, getPos, node, HTMLAttributes }) => {
-      const dom = document.createElement('div')
-      const attributes = mergeAttributes(this.options.HTMLAttributes, HTMLAttributes, {
-        // 'data-type': this.name,
-        level: node.attrs.level
-        // open: node.attrs.open,
-      })
-
-      Object.entries(attributes).forEach(([key, value]) => dom.setAttribute(key, value))
-
-      // get parrent elemen data-id
-      const parent = editor.view.state.doc.nodeAt(getPos() - 1)
-      const headingId = parent.attrs.id || 1
-
-      dom.setAttribute('data-id', headingId)
-
-      const buttonWrapper = document.createElement('div')
-
-      buttonWrapper.classList.add('buttonWrapper')
-
-      // create a button
-      const button = document.createElement('button')
-
-      button.setAttribute('class', 'btn_openChatBox')
-      button.setAttribute('type', 'button')
-      button.contenteditable = false
-      buttonWrapper.append(button)
-
-      // fold and unfold the heading
-      const toggleHeading = document.createElement('button')
-
-      toggleHeading.contentEditable = false
-      toggleHeading.classList.add('unselectable')
-      toggleHeading.classList.add('btnFold')
-      toggleHeading.contenteditable = false
-      buttonWrapper.append(toggleHeading)
-
-      const toggleHeadingContent = (el) => {
-        console.log('what', node.attrs.open)
-        dom.classList.toggle(this.options.openClassName)
-        const detailsContent = document.querySelector(`.title[data-id="${headingId}"] ~ div.contentWrapper`)
-        const event = new CustomEvent('toggleHeadingsContent', { detail: { headingId, open: node.attrs.open, dom, el: detailsContent } })
-
-        console.log(detailsContent)
-        detailsContent === null || detailsContent === void 0 ? void 0 : detailsContent.dispatchEvent(event)
-      }
-
-      const foldAndUnfold = (e) => {
-        const el = e.target
-
-        editor.commands.focus(getPos() + node.nodeSize - 1)
-        if (editor.isEditable && typeof getPos === 'function') {
-          editor
-            .chain()
-            .focus(getPos() + node.nodeSize - 1)
-            .command(({ tr }) => {
-              const pos = getPos()
-              const currentNode = tr.doc.nodeAt(pos)
-
-              console.log(currentNode)
-              if ((currentNode === null || currentNode === void 0 ? void 0 : currentNode.type) !== this.type) {
-                return false
-              }
-              tr.setNodeMarkup(pos, undefined, {
-                open: !currentNode.attrs.open,
-                level: currentNode.attrs.level
-              }).setNodeMarkup(pos - 1, undefined, {
-                open: !currentNode.attrs.open,
-                level: currentNode.attrs.level
-              })
-
-              return true
-            })
-            .run()
-
-          toggleHeadingContent(el)
-        }
-      }
-
-      toggleHeading.addEventListener('click', foldAndUnfold)
-
-      // copy the link to clipboard
-      const href = document.createElement('a')
-
-      href.classList.add('unselectable')
-      href.contentEditable = false
-      href.innerHTML = '#'
-      href.setAttribute('href', `#${headingId}`)
-      href.addEventListener('click', (e) => {
-        e.preventDefault()
-        const url = new URL(window.location)
-
-        url.searchParams.set('id', headingId)
-        window.history.pushState({}, '', url)
-        copyToClipboard(url)
-        editor
-          .chain()
-          .focus(getPos() + node.nodeSize - 1)
-          .run()
-      })
-      href.contenteditable = false
-      buttonWrapper.append(href)
-      dom.append(buttonWrapper)
-
-      // heaidng contnet
-      const content = document.createElement(`h${node.attrs.level}`)
-
-      dom.append(content)
-
-      return {
-        dom,
-        contentDOM: content,
-        ignoreMutation (mutation) {
-          if (mutation.type === 'selection') {
-            return false
-          }
-
-          return !dom.contains(mutation.target) || dom === mutation.target
-        },
-        update: updatedNode => {
-          if (updatedNode.type !== this.type) {
-            return false
-          }
-
-          return true
-        }
       }
     }
   },
@@ -265,6 +241,22 @@ const HeadingsTitle = Node.create({
         }
       }
     }
+  },
+  addProseMirrorPlugins () {
+    return [
+      new Plugin({
+        key: new PluginKey('HeadingButtons'),
+        state: {
+          init: (_, { doc }) => appendButtonsDec(doc, this.editor),
+          apply: (tr, old) => tr.docChanged ? appendButtonsDec(tr.doc, this.editor) : old
+        },
+        props: {
+          decorations (state) {
+            return this.getState(state)
+          }
+        }
+      })
+    ]
   }
 })
 
