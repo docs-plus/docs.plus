550118b09a71a4e3652a8e48458e879c2aadcb26
Hossein Marzban <marzban98@gmail.com>
2023-02-14 11:09:24 +0330

    refactor: get supabase session only on PROD

--- packages/web/src/contexts/Auth.jsx
+++ packages/web/src/contexts/Auth.jsx
@@ -25,36 +25,37 @@ export function AuthProvider ({ children }) {
   const [profile, setProfile] = useState()
   const [loading, setLoading] = useState(false)
 
-  // usememo hook to get GetUser
-  // const getUser = useCallback(() => {
+  // Get the user session when in production
+  if (import.meta.env.PROD) {
+    // usememo hook to get GetUser
+    const getUser = useCallback(() => {
+      (async () => {
+        const { data, error } = await supabase.auth.getUser()
+        const { data: profile, error: profileError } = await supabase
+          .from('users')
+          .select()
+          .eq('id', data?.user?.id)
+          .single()
 
-  //   (async () => {
-  //     const { data, error } = await supabase.auth.getUser()
-  //     let { data: profile, error: profileError } = await supabase
-  //       .from('users')
-  //       .select()
-  //       .eq('id', data?.user?.id)
-  //       .single()
+        setProfile(profile)
+        setUser(data?.user ?? null)
+        setLoading(false)
+      })()
+    }, [])
 
-  //     setProfile(profile)
-  //     setUser(data?.user ?? null)
-  //     setLoading(false)
-  //   })()
+    useEffect(() => {
+      getUser()
+      // Listen for changes on auth state (logged in, signed out, etc.)
+      const listener = supabase.auth.onAuthStateChange(async (event, session) => {
+        setUser(session?.user ?? null)
+        setLoading(false)
+      })
 
-  // }, [])
-
-  useEffect(() => {
-    // getUser()
-    // Listen for changes on auth state (logged in, signed out, etc.)
-    // const listener = supabase.auth.onAuthStateChange(async (event, session) => {
-    //   setUser(session?.user ?? null)
-    //   setLoading(false)
-    // })
-
-    // return () => {
-    //   listener?.subscription?.unsubscribe()
-    // }
-  }, [])
+      return () => {
+        listener?.subscription?.unsubscribe()
+      }
+    }, [])
+  }
 
   // Will be passed down to Signup, Login and Dashboard components
   const value = {
