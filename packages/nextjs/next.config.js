7e137c6561ff904e56f361518eaae76286cbcbbc
Hossein Marzban <marzban98@gmail.com>
2023-04-07 05:22:00 +0330

    chore: eslinter

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -7,11 +7,17 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: false,
-  runtimeCaching
+  runtimeCaching,
+  // disable: process.env.NODE_ENV === 'development',
+  disableDevLogs: true,
+
 })
 
 
 module.exports = withPWA({
   // config
 
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 })
