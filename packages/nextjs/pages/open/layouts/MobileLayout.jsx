f1c34f01add6ecc101c7f2efbb6b2d314a3d126a
Hossein Marzban <marzban98@gmail.com>
2023-04-27 17:05:08 +0330

    fix: viewport hight

--- packages/nextjs/pages/open/layouts/MobileLayout.jsx
+++ packages/nextjs/pages/open/layouts/MobileLayout.jsx
@@ -25,7 +25,6 @@ const MobileLayout = ({ documentTitle, docSlug, docId, provider, editor }) => {
     }
     editor.commands.focus(selectionPos === 0 ? 'start' : selectionPos)
     setShowToolbar(!showToolbar);
-
   }
 
   editor?.on('focus', ({ editor, event }) => {
@@ -44,11 +43,12 @@ const MobileLayout = ({ documentTitle, docSlug, docId, provider, editor }) => {
     if (isKeyboardOpen) {
       console.log("keyboard is open")
       // editor?.setEditable(true)
+      setShowToolbar(true);
     } else {
       // Make the editor read-only
       editor?.setEditable(false)
       console.log("keyboard is closed")
-
+      setShowToolbar(false);
     }
   }, [isKeyboardOpen])
 
@@ -67,7 +67,6 @@ const MobileLayout = ({ documentTitle, docSlug, docId, provider, editor }) => {
       window.scrollTo({ top: 0 });
       document.body.scrollTop = 0;
 
-
       const viewport = event.target;
       const viewportHeight = Math.trunc(viewport.height - viewport.pageTop);
 
@@ -81,38 +80,32 @@ const MobileLayout = ({ documentTitle, docSlug, docId, provider, editor }) => {
         viewportHeight,
         viewport,
         event,
-        // w, h,
         isKeyboardOpen
       })
-
 
       const selection = window?.getSelection()?.anchorNode?.parentElement
 
       if (!selection) return
 
-      if (deviceDetect.is('iPhone')) {
-        if (event.type !== "scroll") {
-          selection?.scrollIntoView({
-            behavior: 'instant', block: 'start'
-          })
-        }
-      } else {
-        if (event.type !== "resize") {
-          selection?.scrollIntoView({
-            behavior: 'instant', block: 'start'
-          })
-        }
-      }
+      // if (deviceDetect.is('iPhone')) {
+      //   if (event.type !== "scroll") {
+      //     selection?.scrollIntoView({
+      //       behavior: 'instant', block: 'start'
+      //     })
+      //   }
+      // } else {
+      //   if (event.type !== "scroll") {
+      //     selection?.scrollIntoView({
+      //       behavior: 'instant', block: 'start'
+      //     })
+      //   }
+      // }
 
     }
 
 
-
-
-
     window.visualViewport.addEventListener('resize', viewportHandler);
     window.visualViewport.addEventListener('scroll', viewportHandler);
-
 
     return () => {
       window.visualViewport.removeEventListener('resize', viewportHandler);
@@ -121,14 +114,11 @@ const MobileLayout = ({ documentTitle, docSlug, docId, provider, editor }) => {
   }, [rendering, editor])
 
 
-
-
-
   return (
     <>
       <HeadSeo title={documentTitle} description="another open docs plus document" />
       <div className={`pad tiptap relative flex  flex-col border-solid ${ isMobile ? "m_mobile" : "m_desktop" }`}>
-        <div className="docTitle  z-50 top-0 bg-white w-full min-h-14 p-2 flex flex-row items-center sm:border-b-0 border-b">
+        <div className="docTitle top-0 bg-white w-full min-h-14 p-2 flex flex-row items-center sm:border-b-0 border-b">
           {docSlug && (
             <PadTitle
               docSlug={docSlug}
@@ -140,7 +130,6 @@ const MobileLayout = ({ documentTitle, docSlug, docId, provider, editor }) => {
         </div>
         <div className={`toolbars ${ isKeyboardOpen ? 'block' : 'hidden' }  w-full bg-white h-auto z-10 sm:block fixed bottom-0 sm:relative`}>
           {editor ? <Toolbar editor={editor} /> : 'Loading...'}
-          {/* <div className='w-full h-11 p-4 bg-slate-600 '></div> */}
         </div>
         <div className="editor w-full h-full flex relative flex-row-reverse align-top ">
           <div
@@ -155,7 +144,7 @@ const MobileLayout = ({ documentTitle, docSlug, docId, provider, editor }) => {
         <div className='nd_modal hidden bottom nd_filterModal w-full h-full fixed top-0 z-30 '>
           <FilterModal />
         </div>
-        <button onClick={toggleToolbar} className={`${ !isKeyboardOpen ? 'block' : 'hidden' } w-14 h-14 z-50 outline-none fixed bottom-12 right-12 rounded-full drop-shadow-md bg-blue-600 flex align-middle items-center justify-center`}>
+        <button onClick={toggleToolbar} className={`btn_bigBluePencil ${ !isKeyboardOpen ? 'block active' : 'hidden' } w-14 h-14 z-50 outline-none fixed bottom-12 right-12 rounded-full drop-shadow-md bg-blue-600 flex align-middle items-center justify-center`}>
           <Pencil />
         </button>
       </div>
