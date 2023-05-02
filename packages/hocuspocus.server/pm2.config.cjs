32528d3d4e8ced7ea54afe47e3807e6efe173bc7
hossein <marzban98@gmail.com>
2023-05-02 22:56:35 +0330

    fix: database .env name

--- packages/hocuspocus.server/pm2.config.cjs
+++ packages/hocuspocus.server/pm2.config.cjs
@@ -3,7 +3,8 @@ module.exports = {
     name: "stage_rest",
     script: "npm run start:production:rest",
     env_production: {
-      NODE_ENV: "production"
+      NODE_ENV: "production",
+      DATABASE_TYPE: "PostgreSQL"
     },
     env_development: {
       NODE_ENV: "development"
@@ -13,7 +14,8 @@ module.exports = {
     name: "stage_ws",
     script: "npm run start:production:ws",
     env_production: {
-      NODE_ENV: "production"
+      NODE_ENV: "production",
+      DATABASE_TYPE: "PostgreSQL"
     },
     env_development: {
       NODE_ENV: "development"
