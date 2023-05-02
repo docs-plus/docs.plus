5eae80c3ec6192d73cc20d4286ad400341f6d1a4
hossein <marzban98@gmail.com>
2023-05-02 18:58:42 +0330

    log

--- packages/nextjs/hooks/useYdocAndProvider.jsx
+++ packages/nextjs/hooks/useYdocAndProvider.jsx
@@ -10,48 +10,48 @@ const useYdocAndProvider = (docId, setLoading) => {
 
   useEffect(() => {
     if (docId) {
-      if (docId) {
-        const ydoc = new Y.Doc();
+      const ydoc = new Y.Doc();
 
-        const colabProvider = new HocuspocusProvider({
-          url: `${ process.env.NEXT_PUBLIC_PROVIDER_URL }`,
-          name: docId,
-          document: ydoc,
-          onStatus: (data) => {
-            console.log('onStatus', data)
-          },
-          onSynced: (data) => {
-            console.log('++onSynced', data)
-            console.log(`++content loaded from Server, pad name: ${ docId }`, provider?.isSynced)
-            if (data?.state) setLoading(false)
-          },
-          documentUpdateHandler: (update) => {
-            console.log('documentUpdateHandler', update)
-          },
-          onDisconnect: (data) => {
-            console.log("onDisconnect", data)
-          },
-          onMessage: (data) => {
-            // console.log('onMessage', data)
-          },
-        })
+      console.log("docID+++++++", docId)
 
-        setProvider(colabProvider)
-        setYdoc(ydoc)
+      const colabProvider = new HocuspocusProvider({
+        url: `${ process.env.NEXT_PUBLIC_PROVIDER_URL }`,
+        name: docId,
+        document: ydoc,
+        onStatus: (data) => {
+          // console.log('onStatus', data)
+        },
+        onSynced: (data) => {
+          // console.log('++onSynced', data)
+          // console.log(`++content loaded from Server, pad name: ${ docId }`, provider?.isSynced)
+          if (data?.state) setLoading(false)
+        },
+        documentUpdateHandler: (update) => {
+          // console.log('documentUpdateHandler', update)
+        },
+        onDisconnect: (data) => {
+          // console.log("onDisconnect", data)
+        },
+        onMessage: (data) => {
+          // console.log('onMessage', data)
+        },
+      })
 
-        // NOTE: This is not working yet, I need reconsider for offline mode
-        // Store the Y document in the browser
-        // const indexDbProvider = new IndexeddbPersistence(
-        //   docId,
-        //   colabProvider.document
-        // )
+      setProvider(colabProvider)
+      setYdoc(ydoc)
 
-        // indexDbProvider.on('synced', () => {
-        //   console.log(`content loaded from indexdb, pad name: ${ docId }`)
-        //   if (!loadedData) return
-        //   setLoadedData(true)
-        // })
-      }
+      // NOTE: This is not working yet, I need reconsider for offline mode
+      // Store the Y document in the browser
+      // const indexDbProvider = new IndexeddbPersistence(
+      //   docId,
+      //   colabProvider.document
+      // )
+
+      // indexDbProvider.on('synced', () => {
+      //   console.log(`content loaded from indexdb, pad name: ${ docId }`)
+      //   if (!loadedData) return
+      //   setLoadedData(true)
+      // })
     }
   }, [docId]);
 
