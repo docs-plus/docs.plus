ae61c3fb728cfe296881c5d54f217714cd75ed15
hossein <marzban98@gmail.com>
2023-05-11 13:01:01 +0330

    update

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -9,7 +9,7 @@ const withPWA = require('next-pwa')({
   skipWaiting: false,
   runtimeCaching,
   // disable: process.env.NODE_ENV === 'development',
-  disableDevLogs: true,
+  // disableDevLogs: true,
 
 })
 
