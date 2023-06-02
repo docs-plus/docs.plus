73bf59d951cf74a7ded49ce3eefb96dcacc564cf
hossein <marzban98@gmail.com>
2023-06-02 14:22:01 +0330

    refactor

--- packages/nextjs/components/pages/panels/profile/sections/avatarUpload.service.js
+++ packages/nextjs/components/pages/panels/profile/sections/avatarUpload.service.js
@@ -1,29 +1,44 @@
-// avatarUpload.service.js
 import { toast } from 'react-hot-toast'
 
-export async function uploadAvatarToStorage(supabaseClient, bucketName, filePath, file) {
-  const { data, error } = await supabaseClient.storage
-    .from(bucketName)
-    .upload(filePath, file)
-
-  if (error) {
-    toast.error('Failed to upload avatar.')
-    throw error
+const uploadAvatarToStorage = async (supabaseClient, bucketName, filePath, file) => {
+  try {
+    const { error, data } = await supabaseClient.storage.from(bucketName).upload(filePath, file, {
+      cacheControl: '3600',
+      upsert: true
+    })
+    if (error) {
+      toast.error('Failed to upload avatar')
+      throw error
+    }
+    return data
+  } catch (error) {
+    toast.error('Failed to upload avatar')
   }
-
-  return data
 }
 
-export async function updateAvatarInDB(supabaseClient, tableName, avatarUrl, userId) {
-  const { data, error } = await supabaseClient
-    .from(tableName)
-    .update({ avatar_url: avatarUrl })
-    .match({ id: userId })
+const removeAvatarFromStorage = async (supabaseClient, bucketName, filePath) => {
+  try {
+    const { error, data } = await supabaseClient.storage.from(bucketName).remove([filePath])
+    if (error) {
+      toast.error('Failed to remove avatar')
+      throw error
+    }
+    return data
+  } catch (error) {
+    toast.error('Failed to remove avatar')
+  }
+}
 
-  if (error) {
+const updateAvatarInDB = async (supabaseClient, tableName, avatarUrl, userId) => {
+  try {
+    const { error, data } = await supabaseClient.from(tableName).update({ avatar_url: avatarUrl }).match({ id: userId })
+    if (error) {
+      toast.error('Failed to update avatar in database.')
+      throw error
+    }
+    return data
+  } catch (error) {
     toast.error('Failed to update avatar in database.')
-    throw error
   }
-
-  return data
 }
+export { updateAvatarInDB, uploadAvatarToStorage, removeAvatarFromStorage }
