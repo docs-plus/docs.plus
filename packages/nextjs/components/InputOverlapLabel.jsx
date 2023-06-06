eaf3b243ef87029e7edeeeb00ca9393fe73820ea
hossein <marzban98@gmail.com>
2023-06-06 18:57:18 +0330

    fix: style rounded

--- packages/nextjs/components/InputOverlapLabel.jsx
+++ packages/nextjs/components/InputOverlapLabel.jsx
@@ -21,7 +21,7 @@ const InputOverlapLabel = forwardRef(
       className
     )
 
-    const iconContainerClasses = `border-r w-12 flex align-middle justify-center items-center ${
+    const iconContainerClasses = `border-r rounded-l-md w-12 flex align-middle justify-center items-center ${
       props.disabled ? 'bg-slate-200 dark:bg-slate-800 border-gray-400' : 'bg-white dark:bg-slate-900'
     }`
 
@@ -42,7 +42,7 @@ const InputOverlapLabel = forwardRef(
           value={value}
           onChange={onChange}
           placeholder=" "
-          className="block p-2 pt-4 w-full text-sm  text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 disabled:rounded-none disabled:bg-slate-200 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          className="block p-2 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 disabled:rounded-none disabled:bg-slate-200 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           {...props}
         />
         <label htmlFor={id} className={labelClasses}>
