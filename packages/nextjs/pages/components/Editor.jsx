783ee2d1f0a16185d65b114e50438e7a94cd5660
hossein <marzban98@gmail.com>
2023-06-08 14:17:08 +0330

    refactor: lint

--- packages/nextjs/pages/components/Editor.jsx
+++ packages/nextjs/pages/components/Editor.jsx
@@ -5,27 +5,17 @@ import { useEditorStateContext } from '../../context/EditorContext'
 
 const Editor = ({ editor }) => {
   const { loading, applyingFilters } = useEditorStateContext()
-
   if (loading || !editor) {
     return (
-      <div
-        className={`ProseMirror tipta__editor loading mb-12 border-t-0 pt-8 sm:mb-0 sm:p-8 px-6 `}>
-        <DocumentSimpleLoader
-          className="!h-auto heading !border-t-0"
-          level="1"
-        />
+      <div className={`ProseMirror tipta__editor loading mb-12 border-t-0 pt-8 sm:mb-0 sm:p-8 px-6 `}>
+        <DocumentSimpleLoader className="!h-auto heading !border-t-0" level="1" />
         <DocumentWithPuctureLoader className="!h-auto heading" level="1" />
         <DocumentSimpleLoader className="!h-auto heading" level="1" />
       </div>
     )
   }
 
-  return (
-    <EditorContent
-      className={`tipta__editor mb-12 sm:mb-0 sm:p-8`}
-      editor={editor}
-    />
-  )
+  return <EditorContent className={`tipta__editor mb-12 sm:mb-0 sm:p-8`} editor={editor} />
 }
 
 export default Editor
