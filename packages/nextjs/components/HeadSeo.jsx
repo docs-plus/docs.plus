9b6f3c4a111ecbf5f3190c2b1c8cf0e6213a5c3e
hossein <marzban98@gmail.com>
2023-06-08 17:06:17 +0330

    refactor

--- packages/nextjs/components/HeadSeo.jsx
+++ packages/nextjs/components/HeadSeo.jsx
@@ -4,31 +4,35 @@ const TITLE = 'Docs Plus'
 const DESCRIPTION =
   'docs.plus is an open-source, real-time collaborative tool that enables communities to share and organize knowledge in a hierarchical manner. Collaborate on documents and share knowledge in a structured, logical way.'
 const URL = 'http://docs.plus'
+const KEYWORDS = 'docs, collaborative, real-time, knowledge, open-source'
+const IMAGE = '/icons/android-chrome-512x512.png'
 
 const HeadSeo = (
-  { title, description, url } = {
+  { title, description, url, keywords } = {
     title: TITLE,
     description: DESCRIPTION,
-    url: URL
+    url: URL,
+    keywords: KEYWORDS
   }
 ) => {
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:url" content={url} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content="icons/logo.svg" />
+      <meta name="twitter:image" content={IMAGE} />
 
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:site_name" content="Docs Plus" />
       <meta property="og:url" content={url} />
-      <meta property="og:image" content="icons/logo.svg" />
+      <meta property="og:image" content={IMAGE} />
     </Head>
   )
 }
