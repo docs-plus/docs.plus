9059b9cdaaf633914b0d0dc259984239077d150d
hossein <marzban98@gmail.com>
2023-06-23 16:30:03 +0330

    migration

---
+++ packages/hocuspocus.server/prisma/migrations/20230621131923_ownerid_to_documentmetadata/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `ownerId` on the `Documents` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Documents" DROP COLUMN "ownerId";
