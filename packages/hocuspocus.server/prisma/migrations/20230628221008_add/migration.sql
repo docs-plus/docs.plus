d4c89807786b7273e4277520253a7fce44ea8213
hossein <marzban98@gmail.com>
2023-06-29 10:05:15 +0330

    fix: readOnly migrate

--- packages/hocuspocus.server/prisma/migrations/20230628221008_add/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `read_only` on the `DocumentMetadata` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "DocumentMetadata" DROP COLUMN "read_only",
-ADD COLUMN     "readOnly" BOOLEAN NOT NULL DEFAULT false;
