b81295dcd6813df7af31d9ead6eec91f0e65bdfe
hossein <marzban98@gmail.com>
2023-07-21 18:14:00 +0330

    chore: lerna, hyperlink, mono repo

--- packages/extension-hyperlink/rollup.config.js
+++ packages/extension-hyperlink/rollup.config.js
@@ -1,60 +1,66 @@
-import sizes from '@atomico/rollup-plugin-sizes'
-import babel from '@rollup/plugin-babel'
-import commonjs from '@rollup/plugin-commonjs'
-import resolve from '@rollup/plugin-node-resolve'
-import autoExternal from 'rollup-plugin-auto-external'
-import sourcemaps from 'rollup-plugin-sourcemaps'
-import typescript from 'rollup-plugin-typescript2'
+// rollup.config.js
+import typescript from "rollup-plugin-typescript2";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import terser from "@rollup/plugin-terser";
+import babel from "@rollup/plugin-babel";
+import autoExternal from "rollup-plugin-auto-external";
+import sourcemaps from "rollup-plugin-sourcemaps";
+import sizes from "@atomico/rollup-plugin-sizes";
+import pkg from "./package.json" assert { type: "json" };
 
-import pkg from './package.json'
-
-export default {
-  external: [/@tiptap\/pm\/.*/],
-  input: 'src/index.ts',
+const config = {
+  external: [/@tiptap\/core\/.*/, /@tiptap\/pm\/.*/],
+  input: "src/index.ts",
   output: [
     {
       name: pkg.name,
       file: pkg.umd,
-      format: 'umd',
+      format: "umd",
       sourcemap: true,
     },
     {
       name: pkg.name,
       file: pkg.main,
-      format: 'cjs',
+      format: "cjs",
       sourcemap: true,
-      exports: 'auto',
+      exports: "auto",
     },
     {
       name: pkg.name,
       file: pkg.module,
-      format: 'es',
+      format: "es",
       sourcemap: true,
     },
   ],
   plugins: [
     autoExternal({
-      packagePath: './package.json',
+      packagePath: "./package.json",
     }),
     sourcemaps(),
     resolve(),
     commonjs(),
     babel({
-      babelHelpers: 'bundled',
-      exclude: '../../node_modules/**',
+      babelHelpers: "bundled",
+      exclude: "../../node_modules/**",
     }),
     sizes(),
     typescript({
-      tsconfig: '../../tsconfig.json',
+      tsconfig: "../../tsconfig.json",
       tsconfigOverride: {
         compilerOptions: {
           declaration: true,
           paths: {
-            '@tiptap/*': ['packages/*/src'],
+            "@docsplus/*": ["packages/*/src"],
           },
         },
-        include: null,
       },
+      include: null,
     }),
+    // json(),
+    // terser(),
   ],
-}
+};
+
+export default config;
--- packages/extension-hyperlink/src/helpers/clickHandler.ts
+++ packages/extension-hyperlink/src/helpers/clickHandler.ts
@@ -1,34 +1,84 @@
-import { getAttributes } from '@tiptap/core'
-import { MarkType } from '@tiptap/pm/model'
-import { Plugin, PluginKey } from '@tiptap/pm/state'
+import { getAttributes } from "@tiptap/core";
+import { MarkType } from "@tiptap/pm/model";
+import { Plugin, PluginKey } from "@tiptap/pm/state";
+import { Editor } from "@tiptap/core";
+import { EditorView } from "@tiptap/pm/view";
+import Tooltip from "./tippyHelper";
 
+// Define type for the ClickHandlerOptions
 type ClickHandlerOptions = {
-  type: MarkType
-}
+  type: MarkType;
+  editor: Editor;
+  validate?: (url: string) => boolean;
+  view: EditorView;
+  modals: {
+    previewHyperlink?: ((options: any) => HTMLElement) | null;
+    setHyperlink?: ((options: any) => HTMLElement) | null;
+  };
+};
 
-export function clickHandler(options: ClickHandlerOptions): Plugin {
+export default function clickHandler(options: ClickHandlerOptions): Plugin {
+  // Create the tooltip instance
+  let tooltip = new Tooltip(options);
+
+  // Initialize the tooltip
+  let { tippyModal, tippyInstance } = tooltip.init();
+
   return new Plugin({
-    key: new PluginKey('handleClickLink'),
+    key: new PluginKey("handleClickHyperlink"),
     props: {
       handleClick: (view, pos, event) => {
-        if (event.button !== 0) {
-          return false
+        if (event.button !== 0) return false;
+
+        // Get the target HTML element and its position
+        const nodeTarget: HTMLElement = event.target as HTMLElement;
+        const nodePos = view.posAtDOM(nodeTarget, 0);
+
+        // Find the closest link element to the target element
+        const link = nodeTarget?.closest("a");
+
+        // Extract attributes from the state
+        const attrs = getAttributes(view.state, options.type.name);
+
+        // Extract href and target attributes from the link element or the state
+        const href = link?.href ?? attrs.href;
+        const target = link?.target ?? attrs.target;
+
+        // If there is no previewHyperlink modal provided, then open the link in new window
+        if (!options.modals.previewHyperlink) {
+          if (link && href) {
+            window.open(href, target);
+          }
+          return true;
         }
 
-        const attrs = getAttributes(view.state, options.type.name)
-        const link = (event.target as HTMLElement)?.closest('a')
+        // if the link does not contain href attribute, hide the tooltip
+        if (!link?.href) return tooltip.hide();
 
-        const href = link?.href ?? attrs.href
-        const target = link?.target ?? attrs.target
+        // Create a preview of the hyperlink
+        const hyperlinkPreview = options.modals.previewHyperlink({
+          link,
+          nodePos,
+          tippy: tooltip,
+          ...options,
+        });
 
-        if (link && href) {
-          window.open(href, target)
+        // If there is no hyperlink preview, hide the modal
+        if (!hyperlinkPreview) return tooltip.hide();
 
-          return true
+        // Empty the modal and append the hyperlink preview box
+
+        while (tippyModal.firstChild) {
+          tippyModal.removeChild(tippyModal.firstChild);
         }
 
-        return false
+        tippyModal.append(hyperlinkPreview);
+
+        // Update the modal position
+        tooltip.update(options.view);
+
+        return false;
       },
     },
-  })
+  });
 }
