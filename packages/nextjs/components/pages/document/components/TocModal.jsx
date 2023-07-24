61de1cc159b083dbe641121106dff032e6787a79
hossein <marzban98@gmail.com>
2023-07-24 18:09:46 +0330

    refactor: remove unused injection

--- packages/nextjs/components/pages/document/components/TocModal.jsx
+++ packages/nextjs/components/pages/document/components/TocModal.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import Link from 'next/link'
-
-import { useEditorStateContext } from '@context/EditorContext'
 import DocTitle from '@components/TipTap/DocTitle'
 import { Filter, DocsPlus } from '@icons'
 import TOC from './Toc'
