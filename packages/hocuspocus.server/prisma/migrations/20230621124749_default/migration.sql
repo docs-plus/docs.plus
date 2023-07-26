ded5b95d12c359dff3d7330c7316e57930e49fd7
hossein <marzban98@gmail.com>
2023-07-26 19:34:29 +0330

    chore(hocuspocus): clean prisma migrations

--- packages/hocuspocus.server/prisma/migrations/20230621124749_default/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "DocumentUsers" ALTER COLUMN "role" DROP NOT NULL,
-ALTER COLUMN "role" SET DEFAULT 'collaborator',
-ALTER COLUMN "email" DROP NOT NULL;
--- packages/hocuspocus.server/prisma/migrations/20230622134023_add_email_to_documentmetadata/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "DocumentMetadata" ADD COLUMN     "email" TEXT;
