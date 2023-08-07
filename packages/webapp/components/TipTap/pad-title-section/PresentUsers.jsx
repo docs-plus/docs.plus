addc32e13598ec2b83ea06a9bdf88980704547c8
hossein <marzban98@gmail.com>
2023-08-07 11:24:10 +0330

    refactor(webapp): improve performance

--- packages/webapp/components/TipTap/pad-title-section/PresentUsers.jsx
+++ packages/webapp/components/TipTap/pad-title-section/PresentUsers.jsx
@@ -1,30 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import AvatarStack from '../../AvatarStack'
 import { useEditorStateContext } from '@context/EditorContext'
 
-const PresentUsers = ({ className, user }) => {
-  const { EditorProvider } = useEditorStateContext()
-  const [presentUsers, setPresentUsers] = useState(null)
-  useEffect(() => {
-    if (!EditorProvider) return
+const PresentUsers = ({ className }) => {
+  const { presentUsers } = useEditorStateContext()
 
-    const awarenessUpdateHandler = ({ states }) => {
-      if (states.length === 0) return
-      if (!user) return setPresentUsers(states)
-      // if user is present, remove it from the list
-      setPresentUsers(() => {
-        return states.filter((x) => x.user?.id !== user.id)
-      })
-    }
-
-    EditorProvider.on('awarenessUpdate', awarenessUpdateHandler)
-
-    return () => {
-      if (EditorProvider) {
-        EditorProvider.off('awarenessUpdate', awarenessUpdateHandler)
-      }
-    }
-  }, [EditorProvider])
   return <div className={className}>{presentUsers && <AvatarStack users={presentUsers} />}</div>
 }
 
