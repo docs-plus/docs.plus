56aa65bfc8a8d6332bf08bbceaeb6d13830dce5f
hossein <marzban98@gmail.com>
2023-08-08 23:03:02 +0330

    refactor(webapp): share doc title state across all instances

--- packages/webapp/components/TipTap/DocTitle.jsx
+++ packages/webapp/components/TipTap/DocTitle.jsx
@@ -1,36 +1,40 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
+import { useDocumentTitle } from '@context/DocumentTitleContext'
 import useUpdateDocMetadata from '../../hooks/useUpdateDocMetadata'
 import toast from 'react-hot-toast'
 
 const DocTitle = ({ className, docMetadata }) => {
   const { isLoading, isSuccess, mutate, data } = useUpdateDocMetadata()
-  const { title, documentId } = docMetadata
+  const { title, setTitle } = useDocumentTitle()
 
-  const [documentTitle, setDocumentTitle] = useState(title)
+  useEffect(() => {
+    setTitle(docMetadata.title)
+  }, [])
+
+  const documentTitle = docMetadata.title || title
 
   const saveData = (e) => {
-    if (e.target.innerText === title) return
+    if (e.target.innerText === documentTitle) return
+
     mutate({
       title: e.target.innerText,
-      documentId
+      documentId: docMetadata.documentId
     })
   }
 
   useEffect(() => {
-    if (isSuccess) {
-      toast.success('Document title change successfully')
-    }
     if (isSuccess && data) {
-      setDocumentTitle(data.data.title)
+      setTitle(data.data.title)
+      toast.success('Document title changed successfully')
     }
-  }, [isSuccess, data])
+  }, [isSuccess])
 
   if (isLoading) return 'Loading...'
 
   return (
     <div className={`${className} `}>
       <div
-        dangerouslySetInnerHTML={{ __html: documentTitle }}
+        dangerouslySetInnerHTML={{ __html: title }}
         contentEditable
         className="border border-transparent px-2 py-0 rounded-sm text-lg font-medium w-full hover:border-slate-300 truncate"
         type="text"
---
+++ packages/webapp/context/DocumentTitleContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useContext, useState } from 'react'
+
+export const DocumentTitleContext = createContext()
+
+export const useDocumentTitle = () => {
+  return useContext(DocumentTitleContext)
+}
+
+// FIXME: not good solution!
+export const DocumentTitleProvider = ({ children }) => {
+  const [title, setDocTitle] = useState()
+
+  const setTitle = (title) => setDocTitle(title)
+
+  return <DocumentTitleContext.Provider value={{ title, setTitle }}>{children}</DocumentTitleContext.Provider>
+}
