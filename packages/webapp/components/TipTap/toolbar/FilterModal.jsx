13f546dd75c047a72a3332c3cca648039efc6b8a
hossein <marzban98@gmail.com>
2023-08-28 09:00:18 +0330

    feat(webapp): highlight toc heaidng in the search filter

--- packages/webapp/components/TipTap/toolbar/FilterModal.jsx
+++ packages/webapp/components/TipTap/toolbar/FilterModal.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
 import { useRouter } from 'next/router'
-import { searchThroughHeading, applySerchThroughHeading } from './toolbarUtils'
 import InputOverlapLabel from '@components/ui/InputOverlapLabel'
+import { searchThroughHeading, applySearchThroughHeading, highlightTocHeadings } from './toolbarUtils'
 
 const FilterModal = ({ totalHeading }) => {
   const router = useRouter()
@@ -10,39 +10,49 @@ const FilterModal = ({ totalHeading }) => {
   const [filterInput, setFilterInput] = useState('')
   const [filteredHeadings, setFilteredHeadings] = useState([])
 
+  useEffect(() => {
+    if (totalHeading !== totalSearch && totalSearch > 0) {
+      highlightTocHeadings(filteredHeadings)
+    } else {
+      highlightTocHeadings([])
+    }
+  }, [totalSearch])
+
   const handleSearch = useCallback(
     (e) => {
       const { totalSearch, totalHeadings, filteredHeadings } = searchThroughHeading(e)
-      setFilteredHeadings(filteredHeadings.map((heading) => heading.textContent))
+      setFilteredHeadings(filteredHeadings)
       setTotalHeadings(totalHeadings)
       setTotalSearch(totalSearch)
+
       if (e.key === 'Enter') {
-        applySerchThroughHeading(filterInput, router)
+        applySearchThroughHeading(filterInput, router)
       }
     },
     [filterInput, router]
   )
 
+  const handleApplySearch = () => {
+    applySearchThroughHeading(filterInput, router)
+  }
+
   return (
     <div className="filterModal nd_modal">
       <div className="flex align-middle items-center">
         <p className="font-medium text-base text-gray-400 pb-1">Filter:</p>
-        <button
-          onClick={() => applySerchThroughHeading(filterInput, router)}
-          className="!p-3 !w-24 !ml-auto  border">
+        <button onClick={handleApplySearch} className="p-3 w-24 ml-auto border">
           <span>Apply</span>
         </button>
       </div>
-
       <hr />
       <div className="content pt-2 flex align-middle justify-between">
         <InputOverlapLabel
-          className="h-10 "
+          className="h-10"
           id="filterSearchBox"
           label="Find in document"
           value={filterInput}
           onKeyUp={handleSearch}
-          datalist={filteredHeadings}
+          datalist={filteredHeadings.map((heading) => heading.textContent)}
           onChange={(e) => setFilterInput(e.target.value)}
         />
         <p className="mx-2 text-xs text-center">
