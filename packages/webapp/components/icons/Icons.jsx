acd38995118c7e65ff67bacd1971c4d781b42fd7
hossein <marzban98@gmail.com>
2023-09-07 16:05:49 +0330

    feat(filter): display filter chips close to pad title

--- packages/webapp/components/icons/Icons.jsx
+++ packages/webapp/components/icons/Icons.jsx
@@ -623,3 +623,13 @@ export const CaretRight = ({ size = 24, fill = 'rgb(104, 81, 255)', className })
     </svg>
   )
 }
+
+// prettier-ignore
+export const Close = ({ size = 24, fill = 'black', className }) => {
+  return (
+    <svg className={className} fill={fill} viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" width={size} xmlns="http://www.w3.org/2000/svg">
+      <line x1="18" y1="6" x2="6" y2="18"></line>
+      <line x1="6" y1="6" x2="18" y2="18"></line>
+    </svg>
+  )
+}
