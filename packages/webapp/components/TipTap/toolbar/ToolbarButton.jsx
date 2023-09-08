d136d978e10d87c18d5ab7b8e9e92c19052b6447
hossein <marzban98@gmail.com>
2023-09-08 16:49:34 +0330

    refactor(webapp): add tooltip to ToolbarButton

--- packages/webapp/components/TipTap/toolbar/ToolbarButton.jsx
+++ packages/webapp/components/TipTap/toolbar/ToolbarButton.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
-const ToolbarButton = ({ type, editor, onClick, children }) => {
+import { Tooltip, TooltipTrigger, TooltipContent } from '@components/ui/Tooltip'
+
+const ToolbarButton = ({ type, editor, onClick, children, tooltip }) => {
   const buttonClass = editor?.isActive(type) ? 'is-active' : ''
 
   return (
-    <button className={buttonClass} onClick={onClick}>
-      {children}
-    </button>
+    <Tooltip placement="bottom">
+      <TooltipTrigger asChild={true}>
+        <button className={buttonClass} onClick={onClick}>
+          {children}
+        </button>
+      </TooltipTrigger>
+      <TooltipContent className="Tooltip">{tooltip}</TooltipContent>
+    </Tooltip>
   )
 }
 
