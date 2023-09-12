e6e662962f292a0834282ffb189e4417d9e896c5
hossein <marzban98@gmail.com>
2023-09-13 01:23:16 +0330

    feat(filter): toggle filter algorithm

--- packages/webapp/components/TipTap/toolbar/FilterModal.jsx
+++ packages/webapp/components/TipTap/toolbar/FilterModal.jsx
@@ -5,12 +5,16 @@ import { twMerge } from 'tailwind-merge'
 import { searchThroughHeading, applySearchThroughHeading, highlightTocHeadings } from './toolbarUtils'
 import { PopoverContent } from '@components/ui/Popover'
 import Button from '@components/ui/Button'
+import Toggle from '@components/ui/Toggle'
+import { useBooleanLocalStorageState } from './toolbarUtils'
+
 const FilterModal = ({ totalHeading, className }) => {
   const router = useRouter()
   const [totalSearch, setTotalSearch] = useState(0)
   const [totalHeadings, setTotalHeadings] = useState(totalHeading)
   const [filterInput, setFilterInput] = useState('')
   const [filteredHeadings, setFilteredHeadings] = useState([])
+  const [filterAlgorithm, setFilterAlgorithm] = useBooleanLocalStorageState('setting.filterAlgorithm', false)
 
   useEffect(() => {
     if (totalHeading !== totalSearch && totalSearch > 0) {
@@ -38,6 +42,29 @@ const FilterModal = ({ totalHeading, className }) => {
     applySearchThroughHeading(filterInput, router)
   }
 
+  const handelFilterAlgorithm = () => {
+    setFilterAlgorithm(!filterAlgorithm)
+    if (router.query.slugs.length > 0) {
+      window.location.href = `${location.origin}/${router.asPath}`
+    }
+  }
+
+  const ToggleSection = ({ name, className, description, value, checked, onChange }) => {
+    const containerClasses = twMerge(`flex flex-col p-2 antialiased `, className)
+
+    return (
+      <div className={containerClasses}>
+        <p className="text-base font-bold">{name}</p>
+        <div className="flex w-full flex-row align-middle justify-between items-center">
+          <p className="text-gray-500 text-sm">{description}</p>
+          <div className="border-l flex-col h-full py-2 px-3 ml-2 mr-6">
+            <Toggle id={value} checked={checked} onChange={onChange} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <PopoverContent className={twMerge('Popover gearModal', className)}>
       <div className="flex align-middle items-center">
@@ -47,7 +74,7 @@ const FilterModal = ({ totalHeading, className }) => {
         </Button>
       </div>
       <hr />
-      <div className="content pt-2 flex align-middle justify-between">
+      <div className="content pt-2 flex align-middle justify-between pb-2 border-b">
         <InputOverlapLabel
           className="h-10"
           id="filterSearchBox"
@@ -61,6 +88,12 @@ const FilterModal = ({ totalHeading, className }) => {
           Found <b>{totalSearch}</b> matches out of <b>{totalHeadings}</b> headings
         </p>
       </div>
+      <ToggleSection
+        name="Filter Algorithm"
+        description="Switch between relevant-only OR sequential heading filtering"
+        checked={filterAlgorithm}
+        onChange={handelFilterAlgorithm}
+      />
     </PopoverContent>
   )
 }
