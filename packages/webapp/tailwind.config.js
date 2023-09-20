f1fd2c2397e3aa8f21d9ecfe80758e1ee793eeec
hossein <marzban98@gmail.com>
2023-09-20 19:34:42 +0330

    chore(core): review & remove dependencies

--- packages/webapp/tailwind.config.js
+++ packages/webapp/tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-import colors from 'tailwindcss/colors'
+
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
 
     // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   plugins: [],
   darkMode: 'class',
