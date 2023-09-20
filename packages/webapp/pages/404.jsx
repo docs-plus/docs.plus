e560894c0ed94a4d0293c0067a4a9bc802207ba7
hossein <marzban98@gmail.com>
2023-09-20 17:35:28 +0330

    feat(webapp): 500 error page, when server is unavailable

--- packages/webapp/pages/404.jsx
+++ packages/webapp/pages/404.jsx
@@ -1,8 +1,8 @@
 import Link from 'next/link'
 export default function Custom404() {
   return (
-    <div className="h-screen max-w-5xl m-auto flex flex-col justify-center align-middle p-4">
-      <div className=" p-2 w-full justify-self-center flex justify-center flex-col">
+    <div className="h-screen w-full m-auto flex flex-col justify-center items-center p-4 bg-gray-100">
+      <div className="p-4 w-2/6 text-center bg-white shadow-xl rounded-lg">
         <p className="text-center text-lg">Oops! You seem to be lost. 404 </p>
         <p className="text-center mt-4">
           <Link href="/">Home</Link>
