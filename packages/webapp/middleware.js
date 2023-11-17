33a437555ff4c4ec7b9354994ee0102c02546715
hossein <marzban98@gmail.com>
2023-11-17 23:26:36 +0330

    update

--- packages/webapp/middleware.js
+++ packages/webapp/middleware.js
@@ -22,10 +22,10 @@
 //   }
 // }
 
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
-import { NextResponse, type NextRequest } from 'next/server'
+import { createServerClient } from '@supabase/ssr'
+import { NextResponse } from 'next/server'
 
-export async function middleware(request: NextRequest) {
+export async function middleware(request) {
   let response = NextResponse.next({
     request: {
       headers: request.headers
@@ -33,14 +33,14 @@ export async function middleware(request: NextRequest) {
   })
 
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
       cookies: {
-        get(name: string) {
+        get(name) {
           return request.cookies.get(name)?.value
         },
-        set(name: string, value: string, options: CookieOptions) {
+        set(name, value, options) {
           request.cookies.set({
             name,
             value,
@@ -57,7 +57,7 @@ export async function middleware(request: NextRequest) {
             ...options
           })
         },
-        remove(name: string, options: CookieOptions) {
+        remove(name, options) {
           request.cookies.set({
             name,
             value: '',
