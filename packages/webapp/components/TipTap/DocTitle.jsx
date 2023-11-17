786c04843919b692f994d90610d99029b0661f62
hossein <marzban98@gmail.com>
2023-11-17 22:27:06 +0330

    refactor(webapp): add real-time title updates using SSE

--- packages/webapp/components/TipTap/DocTitle.jsx
+++ packages/webapp/components/TipTap/DocTitle.jsx
@@ -1,22 +1,49 @@
-import { useEffect } from 'react'
-import { useDocumentTitle } from '@context/DocumentTitleContext'
+import { useEffect, useState } from 'react'
 import useUpdateDocMetadata from '../../hooks/useUpdateDocMetadata'
 import toast from 'react-hot-toast'
 import { useEditorStateContext } from '@context/EditorContext'
+import { useDocumentMetadataContext } from '@context/DocumentMetadataContext'
+const SSE_TITLE_ADDRESS = (docId) => `${process.env.NEXT_PUBLIC_SSE_URL}/${docId}_docTitle`
 
-const DocTitle = ({ className, docMetadata }) => {
+const broadcastTitle = async (documentId, newTitle) => {
+  await fetch(SSE_TITLE_ADDRESS(documentId), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ title: newTitle })
+  })
+}
+
+const DocTitle = ({ className }) => {
   const { isLoading, isSuccess, mutate, data } = useUpdateDocMetadata()
-  const { title, setTitle } = useDocumentTitle()
+  const [title, setTitle] = useState()
   const { EditorProvider } = useEditorStateContext()
+  const docMetadata = useDocumentMetadataContext()
 
   useEffect(() => {
     setTitle(docMetadata.title)
+  }, [])
+
+  useEffect(() => {
+    const eventSource = new EventSource(SSE_TITLE_ADDRESS(docMetadata.documentId))
+
+    eventSource.onmessage = (event) => {
+      const data = JSON.parse(event.data)
+      if (data.body.title) setTitle(data.title)
+    }
+
+    return () => {
+      eventSource.close()
+    }
   }, [])
 
   const documentTitle = docMetadata.title || title
 
   const saveData = (e) => {
     if (e.target.innerText === documentTitle) return
+
+    broadcastTitle(docMetadata.documentId, e.target.innerText)
 
     mutate({
       title: e.target.innerText,
