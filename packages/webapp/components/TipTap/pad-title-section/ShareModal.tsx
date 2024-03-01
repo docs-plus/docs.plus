100903a076893b5e75acdc29501ef5e964ff50ce
hossein <marzban98@gmail.com>
2024-03-01 11:12:05 +0330

    style(webapp): redesign close btn

--- packages/webapp/components/TipTap/pad-title-section/ShareModal.tsx
+++ packages/webapp/components/TipTap/pad-title-section/ShareModal.tsx
@@ -5,6 +5,7 @@ import { useStore } from '@stores'
 import { BsWhatsapp } from 'react-icons/bs'
 import { BsReddit } from 'react-icons/bs'
 import { FiCopy } from 'react-icons/fi'
+import { IoCloseSharp } from 'react-icons/io5'
 
 const socialSharingMap = {
   facebook: `https://www.facebook.com/sharer.php?u=`,
@@ -60,20 +61,8 @@ const ShareModal = ({ setIsOpen }: any) => {
     <div className=" p-4 pt-2 flex flex-col">
       <div className="flex mb-6">
         <p className="text-2xl font-semibold ">Share</p>
-        <button className="btn btn-circle btn-sm ml-auto" onClick={() => setIsOpen(false)}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
+        <button className="btn btn-circle btn-xs ml-auto" onClick={() => setIsOpen(false)}>
+          <IoCloseSharp size={20} />
         </button>
       </div>
       <div className="flex space-x-4 justify-around">
