9069d5f91026a1121584d5edd3aff588d2b7f5ac
hossein <marzban98@gmail.com>
2024-03-02 15:31:44 +0330

    refactor: replace supabaseClint package with @supabase/auth-helpers-nextjs

--- packages/webapp/utils/supabase.js
+++ packages/webapp/utils/supabase.js
@@ -1,12 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
 // Initialize Supabase
-export const supabaseClient = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  {
-    auth: {
-      persistSession: true
-    }
-  }
-)
+export const supabaseClient = createClientComponentClient()
