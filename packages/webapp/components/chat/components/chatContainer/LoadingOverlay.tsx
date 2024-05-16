539d748a9ce508e84559b9daf13a872a7a4b8a93
hossein <marzban98@gmail.com>
2024-05-16 20:08:39 +0330

    fix: loader for chat pannel

--- packages/webapp/components/chat/components/chatContainer/LoadingOverlay.tsx
+++ packages/webapp/components/chat/components/chatContainer/LoadingOverlay.tsx
@@ -3,9 +3,9 @@ export const LoadingOverlay = ({ loading }: { loading: boolean }) => {
 
   return (
     <div
-      className="absolute z-50 flex h-dvh w-full items-center justify-center bg-base-100"
+      className="absolute z-50 flex size-full items-center justify-center bg-base-100"
       style={{ display: loading ? 'flex' : 'none' }}>
-      <div className="flex h-dvh w-full items-center justify-center">
+      <div className="flex w-full items-center justify-center">
         <span className="loading loading-spinner text-primary"></span>
       </div>
     </div>
--- packages/webapp/components/chat/components/prompts/ChannelLoadingPrompt.tsx
+++ packages/webapp/components/chat/components/prompts/ChannelLoadingPrompt.tsx
@@ -2,7 +2,7 @@ import { LoadingOverlay } from '../chatContainer'
 
 export const ChannelLoadingPrompt = ({ loading }: { loading: boolean }) => {
   return (
-    <div className="relative flex h-dvh w-full max-w-full flex-col items-center justify-start bg-base-300">
+    <div className="relative flex size-full max-w-full flex-1 flex-col items-center justify-start bg-base-300">
       <LoadingOverlay loading={loading} />
     </div>
   )
