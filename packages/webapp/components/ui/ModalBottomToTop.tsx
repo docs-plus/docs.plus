6d03c65431a698158f9c83c69b0abe20b88c5648
hossein <marzban98@gmail.com>
2024-06-29 12:13:27 +0330

    Fix: stick the modal to the bottom when the keyboard is open.

--- packages/webapp/components/ui/ModalBottomToTop.tsx
+++ packages/webapp/components/ui/ModalBottomToTop.tsx
@@ -1,20 +1,26 @@
+import { twMerge } from 'tailwind-merge'
+
 interface ModalBottomToTopProps {
   modalId?: string
+  className?: string
   children: React.ReactNode
 }
 
 export const ModalBottomToTop: React.FC<ModalBottomToTopProps> = ({
   modalId = 'bottom_to_top_modal',
+  className,
   children
 }) => {
   return (
-    <div className="bottom-to-top-modal">
+    <div className={twMerge('bottom-to-top-modal', className)}>
       <input type="checkbox" id={modalId} className="peer modal-toggle hidden" />
 
-      <div className="modal-overlay pointer-events-none fixed inset-0 z-50 flex items-end justify-center opacity-0 transition-all duration-300 peer-checked:pointer-events-auto peer-checked:opacity-100">
-        <label htmlFor={modalId} className="modal-backdrop absolute inset-0 bg-[#0006]"></label>
+      <div className="modal-overlay pointer-events-none absolute inset-0 z-50 flex h-full items-end justify-center opacity-0 transition-all duration-300 peer-checked:pointer-events-auto peer-checked:opacity-100">
+        <label
+          htmlFor={modalId}
+          className="modal-backdrop absolute inset-0 h-full bg-[#0006]"></label>
 
-        <div className="modal-content max-h-[90vh] w-full max-w-md translate-y-full overflow-y-auto rounded-t-lg bg-white shadow-lg transition-transform duration-300 ease-in-out peer-checked:translate-y-0">
+        <div className="modal-content sticky bottom-0 max-h-[90vh] w-full max-w-md translate-y-full overflow-y-auto rounded-t-lg bg-white shadow-lg transition-transform duration-300 ease-in-out peer-checked:translate-y-0">
           {children}
         </div>
       </div>
