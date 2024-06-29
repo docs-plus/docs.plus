91df2b5f656716376d288e4d1b20ceb454d19a58
hossein <marzban98@gmail.com>
2024-06-29 15:23:24 +0330

    refactor: clean up and move drawer one layer up.

--- packages/webapp/components/pages/document/layouts/MobileLayout.tsx
+++ packages/webapp/components/pages/document/layouts/MobileLayout.tsx
@@ -3,6 +3,26 @@ import MobilePadTitle from '@components/TipTap/pad-title-section/MobilePadTitle'
 import FilterModal from '../components/FilterModal'
 import MobileEditor from '../components/MobileEditor'
 import { useStore } from '@stores'
+import TocModal from '@components/pages/document/components/TocModal'
+
+const MobileLeftSidePanel = () => {
+  return (
+    <div className="drawer z-30 w-full">
+      <input id="mobile_left_side_panel" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-side">
+        <label
+          htmlFor="mobile_left_side_panel"
+          aria-label="close sidebar"
+          className="drawer-overlay"></label>
+        <div className="min-h-full w-full">
+          <label htmlFor="mobile_left_side_panel">
+            <TocModal />
+          </label>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 const MobileLayout = () => {
   const {
@@ -18,6 +38,7 @@ const MobileLayout = () => {
       <div className="docTitle sticky top-0 z-10 flex min-h-14 w-full flex-row items-center bg-white">
         <MobilePadTitle />
       </div>
+      <MobileLeftSidePanel />
       <MobileEditor />
       <FilterModal />
     </div>
