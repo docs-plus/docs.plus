4feb18f341c68f6151e73daca33e768d91c8238f
hossein <marzban98@gmail.com>
2024-11-13 11:53:37 +0330

    chore: befor push, run lerna build to make sure there is no error

--- pre-push.js
+++ pre-push.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process')
 
 try {
   console.log('Running Next.js build...')
-  execSync('cd packages/webapp && npm run build', { stdio: 'inherit' })
+  execSync('npm run build', { stdio: 'inherit' })
   console.log('Build successful. Proceeding with push.')
   process.exit(0)
 } catch (error) {
