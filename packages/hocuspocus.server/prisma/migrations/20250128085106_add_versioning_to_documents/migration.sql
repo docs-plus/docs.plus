23e01c58fba0400dea7b2fee7d089811ea0ebca7
hossein <marzban98@gmail.com>
2025-02-01 17:25:10 +0330

    migration

--- packages/hocuspocus.server/prisma/migrations/20250128085106_add_versioning_to_documents/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Documents" ADD COLUMN     "commitMessage" TEXT,
-ADD COLUMN     "version" INTEGER NOT NULL DEFAULT 1;
-
--- CreateIndex
-CREATE INDEX "Documents_documentId_version_idx" ON "Documents"("documentId", "version");
