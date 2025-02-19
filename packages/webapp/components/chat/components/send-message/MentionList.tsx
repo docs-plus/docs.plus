39e14e7c4cebca4f6e43fad5bfb19796d55d5da0
hossein <marzban98@gmail.com>
2025-02-19 11:31:41 +0330

    feat: mention everyone

--- packages/webapp/components/chat/components/send-message/MentionList.tsx
+++ packages/webapp/components/chat/components/send-message/MentionList.tsx
@@ -5,6 +5,15 @@ import { searchWorkspaceUsers } from '@api'
 import { useApi } from '@hooks/useApi'
 import React, { forwardRef, use, useEffect, useImperativeHandle, useState } from 'react'
 import { useChatStore, useStore } from '@stores'
+import { MdGroups } from 'react-icons/md'
+import { Avatar } from '@components/ui/Avatar'
+
+const everyoneOption = {
+  id: 0,
+  username: 'everyone',
+  full_name: 'All Users',
+  isEveryoneOption: true
+}
 
 export default forwardRef((props, ref) => {
   const [selectedIndex, setSelectedIndex] = useState(0)
@@ -24,7 +33,8 @@ export default forwardRef((props, ref) => {
         console.error(error)
         return
       }
-      setWorkspaceUsers(data)
+
+      setWorkspaceUsers([everyoneOption, ...data])
     }
     fetchWorkspaceUsers()
   }, [workspaceId])
@@ -79,15 +89,31 @@ export default forwardRef((props, ref) => {
   }))
 
   return (
-    <div className="dropdown-menu">
+    <div className="dropdown-menu !p-1">
       {workspaceUsers.length ? (
         workspaceUsers.map((item, index) => (
-          <button
-            className={index === selectedIndex ? 'is-selected' : ''}
-            key={index}
-            onClick={() => selectItem(index)}>
-            {item.username}
-          </button>
+          <React.Fragment key={item.id}>
+            <button
+              className={`flex items-center ${index === selectedIndex ? 'is-selected' : ''}`}
+              onClick={() => selectItem(index)}>
+              {item.isEveryoneOption ? (
+                <MdGroups className="mr-2 size-8 text-gray-600" />
+              ) : (
+                <Avatar
+                  src={item.avatar_url}
+                  avatarUpdatedAt={item.avatar_updated_at}
+                  alt={item.full_name}
+                  size="sm"
+                  className="mr-2 size-8"
+                />
+              )}
+              <span className="flex flex-col justify-start text-sm">
+                {item.full_name}
+                <span className="text-muted text-xs text-gray-600">@{item.username}</span>
+              </span>
+            </button>
+            {index === 0 && <div className="divider m-0 h-2 p-0"></div>}
+          </React.Fragment>
         ))
       ) : (
         <div className="item">No result</div>
