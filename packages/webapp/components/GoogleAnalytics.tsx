8c48a45e5b7466dfd60cf58ba4a90e5c1bbde315
hossein <marzban98@gmail.com>
2025-04-28 17:37:16 +0330

    feat: google analytics

---
+++ packages/webapp/components/GoogleAnalytics.tsx
@@ -0,0 +1,24 @@
+import Script from 'next/script'
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID
+
+export default function GoogleAnalytics() {
+  if (!GA_ID) return null
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
+    </>
+  )
+}
