dccb8d305914c5b78b2d6973900b32ba8a469b0e
hossein <marzban98@gmail.com>
2025-04-29 12:12:38 +0330

    refactor: run google analytics in prod and set headers for gtm in nextjs config

--- packages/webapp/components/GoogleAnalytics.tsx
+++ packages/webapp/components/GoogleAnalytics.tsx
@@ -1,9 +1,10 @@
 import Script from 'next/script'
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID
+const isProduction = process.env.NODE_ENV === 'production'
 
 export default function GoogleAnalytics() {
-  if (!GA_ID) return null
+  if (!GA_ID || !isProduction) return null
 
   return (
     <>
