6b3f5773fa9e71e28c8db62ffd372ba78bccad67
hossein <marzban98@gmail.com>
2025-05-07 12:02:46 +0330

    feat: chatroom gripper

--- packages/webapp/components/pages/document/components/chat/ChatContainer.tsx
+++ packages/webapp/components/pages/document/components/chat/ChatContainer.tsx
@@ -22,11 +22,15 @@ const ChatContainer = () => {
   return (
     <div
       ref={gripperRef}
-      className="group absolute bottom-0 z-40 flex h-[410px] w-full flex-row flex-wrap bg-slate-50"
+      className="group absolute bottom-0 z-40 flex h-[410px] w-full flex-row flex-wrap border-t border-gray-300 bg-white pt-0.5"
       style={{ height: `${height}px` }}>
       <div
-        className="gripper hover:bg-neutral absolute top-0 left-0 z-[51] w-full cursor-ns-resize border-t border-gray-300 transition-all duration-300 group-hover:h-[4px] hover:border-white hover:opacity-90"
+        className="gripper absolute -top-5 left-0 z-[51] w-full bg-transparent pt-10"
+        // onMouseDown={handleMouseDown}
+      />
+      <div
         onMouseDown={handleMouseDown}
+        className="active:bg-block absolute top-2 left-1/2 z-[52] h-1.5 w-22 -translate-x-1/2 cursor-ns-resize rounded-full bg-gray-400 transition-all duration-200 group-hover:opacity-100"
       />
       <div className="flex size-full flex-col justify-start">
         <Toolbar />
