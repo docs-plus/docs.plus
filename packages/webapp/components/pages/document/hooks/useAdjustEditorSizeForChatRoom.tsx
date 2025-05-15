cb69d6c44cb6a696dfde0bd82f2b450df6e3e9dd
hossein <marzban98@gmail.com>
2025-05-15 14:26:57 +0330

    fix: update editor height on chatroom resize

--- packages/webapp/components/pages/document/hooks/useAdjustEditorSizeForChatRoom.tsx
+++ packages/webapp/components/pages/document/hooks/useAdjustEditorSizeForChatRoom.tsx
@@ -1,8 +1,8 @@
 import { useEffect, RefObject } from 'react'
-import { useStore } from '@stores'
+import { useChatStore } from '@stores'
 
 export const useAdjustEditorSizeForChatRoom = (editorWrapperRef: RefObject<HTMLDivElement>) => {
-  const chatRoom = useStore((state) => state.chatRoom)
+  const chatRoom = useChatStore((state) => state.chatRoom)
 
   // adjust the editor size base on the chat room pannel height
   useEffect(() => {
