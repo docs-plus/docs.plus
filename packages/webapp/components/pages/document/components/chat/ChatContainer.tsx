ab4d9b1246e7920c26cb8633938f130118098d6d
hossein <marzban98@gmail.com>
2025-06-13 00:58:29 +0330

    style: add top shadow to chat container

--- packages/webapp/components/pages/document/components/chat/ChatContainer.tsx
+++ packages/webapp/components/pages/document/components/chat/ChatContainer.tsx
@@ -22,7 +22,7 @@ const ChatContainer = () => {
   return (
     <div
       ref={gripperRef}
-      className="group absolute bottom-0 z-40 flex h-[410px] w-full flex-row flex-wrap border-t border-gray-300 bg-white pt-0.5"
+      className="group absolute bottom-0 z-40 flex h-[410px] w-full flex-row flex-wrap border-t border-gray-300 bg-white pt-0.5 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1),0_2px_4px_-1px_rgba(0,0,0,0.06)]"
       style={{ height: `${height}px` }}>
       <div
         className="gripper absolute -top-5 left-0 z-[51] w-full bg-transparent pt-10"
