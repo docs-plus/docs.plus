b5a8baa092a8231a0ffc5a97be03c1ee256a61f2
hossein <marzban98@gmail.com>
2025-09-20 19:24:55 +0330

    chore: update dependencies and refactor BottomSheet component - Upgraded `react-modal-sheet` from 4.4.0 to 5.1.2 for improved functionality. - Added `react-use-measure` as a dependency for better measurement handling. - Refactored `BottomSheet` component to adjust snap points and remove unused code for cleaner implementation. - Updated `ChatContainerMobile` to enhance layout and ensure proper height handling.

--- packages/webapp/components/pages/document/components/chat/ChatContainerMobile.tsx
+++ packages/webapp/components/pages/document/components/chat/ChatContainerMobile.tsx
@@ -35,62 +35,57 @@ const ChatContainerMobile = () => {
   return (
     <>
       <ChannelProvider initChannelId={chatRoom.headingId} initSettings={initSettings}>
-        <Chatroom variant="mobile" className="flex h-auto flex-auto flex-col overflow-hidden">
+        <Chatroom variant="mobile" className="flex h-full flex-auto flex-col overflow-hidden">
           <div className="border-b border-gray-200 bg-gray-100 py-1">
             <BreadcrumbMobile />
           </div>
           <Chatroom.MessageFeed showScrollToBottom={true}>
-            <Sheet.Scroller
-              draggableAt="bottom"
-              disableScroll={chatRoom.disableScroll}
-              autoPadding={true}>
-              <Chatroom.MessageFeed.MessageList>
-                <Chatroom.MessageFeed.MessageList.Loop>
-                  {(message, index) => (
-                    <Chatroom.MessageFeed.MessageList.MessageCard.LongPressMenu message={message}>
-                      <Chatroom.MessageFeed.MessageList.MessageCard
-                        className="max-w-[90%] min-w-[80%] sm:min-w-[250px]"
-                        message={message}
-                        index={index}>
-                        {message.isGroupStart && !message.isOwner && (
-                          <div className="chat-image avatar">
-                            <Chatroom.MessageFeed.MessageList.MessageCard.Header.UserAvatar />
-                          </div>
-                        )}
-
-                        <div
-                          className={`chat-bubble px-2.5 ${message.isOwner && 'bg-chatBubble-owner before:content-[before:content-[]]'} ${!message.isGroupStart && 'ml-9 before:content-[before:content-[]]'}`}>
-                          <Chatroom.MessageFeed.MessageList.MessageCard.Header.BookmarkIndicator />
-                          <Chatroom.MessageFeed.MessageList.MessageCard.Header className="chat-header">
-                            {!message.isOwner && message.isGroupStart && (
-                              <Chatroom.MessageFeed.MessageList.MessageCard.Header.Username />
-                            )}
-                          </Chatroom.MessageFeed.MessageList.MessageCard.Header>
-
-                          <Chatroom.MessageFeed.MessageList.MessageCard.Content className="">
-                            <Chatroom.MessageFeed.MessageList.MessageCard.Content.ReplyReference />
-                            <Chatroom.MessageFeed.MessageList.MessageCard.Content.CommentReference />
-                            <Chatroom.MessageFeed.MessageList.MessageCard.Content.MessageBody />
-                          </Chatroom.MessageFeed.MessageList.MessageCard.Content>
-
-                          <Chatroom.MessageFeed.MessageList.MessageCard.Footer className="chat-footer justify-end">
-                            <Chatroom.MessageFeed.MessageList.MessageCard.Footer.Reactions>
-                              <Chatroom.MessageFeed.MessageList.MessageCard.Footer.Reactions.ReactionList />
-                            </Chatroom.MessageFeed.MessageList.MessageCard.Footer.Reactions>
-                            <Chatroom.MessageFeed.MessageList.MessageCard.Footer.Indicators className="pr-0">
-                              <Chatroom.MessageFeed.MessageList.MessageCard.Footer.Indicators.EditedBadge />
-                              <Chatroom.MessageFeed.MessageList.MessageCard.Footer.Indicators.ReplyCount />
-                              <Chatroom.MessageFeed.MessageList.MessageCard.Header.Timestamp />
-                              <Chatroom.MessageFeed.MessageList.MessageCard.Footer.Indicators.MessageSeen />
-                            </Chatroom.MessageFeed.MessageList.MessageCard.Footer.Indicators>
-                          </Chatroom.MessageFeed.MessageList.MessageCard.Footer>
+            <Chatroom.MessageFeed.MessageList>
+              <Chatroom.MessageFeed.MessageList.Loop>
+                {(message, index) => (
+                  <Chatroom.MessageFeed.MessageList.MessageCard.LongPressMenu message={message}>
+                    <Chatroom.MessageFeed.MessageList.MessageCard
+                      className="max-w-[90%] min-w-[80%] sm:min-w-[250px]"
+                      message={message}
+                      index={index}>
+                      {message.isGroupStart && !message.isOwner && (
+                        <div className="chat-image avatar">
+                          <Chatroom.MessageFeed.MessageList.MessageCard.Header.UserAvatar />
                         </div>
-                      </Chatroom.MessageFeed.MessageList.MessageCard>
-                    </Chatroom.MessageFeed.MessageList.MessageCard.LongPressMenu>
-                  )}
-                </Chatroom.MessageFeed.MessageList.Loop>
-              </Chatroom.MessageFeed.MessageList>
-            </Sheet.Scroller>
+                      )}
+
+                      <div
+                        className={`chat-bubble px-2.5 ${message.isOwner && 'bg-chatBubble-owner before:content-[before:content-[]]'} ${!message.isGroupStart && 'ml-9 before:content-[before:content-[]]'}`}>
+                        <Chatroom.MessageFeed.MessageList.MessageCard.Header.BookmarkIndicator />
+                        <Chatroom.MessageFeed.MessageList.MessageCard.Header className="chat-header">
+                          {!message.isOwner && message.isGroupStart && (
+                            <Chatroom.MessageFeed.MessageList.MessageCard.Header.Username />
+                          )}
+                        </Chatroom.MessageFeed.MessageList.MessageCard.Header>
+
+                        <Chatroom.MessageFeed.MessageList.MessageCard.Content className="">
+                          <Chatroom.MessageFeed.MessageList.MessageCard.Content.ReplyReference />
+                          <Chatroom.MessageFeed.MessageList.MessageCard.Content.CommentReference />
+                          <Chatroom.MessageFeed.MessageList.MessageCard.Content.MessageBody />
+                        </Chatroom.MessageFeed.MessageList.MessageCard.Content>
+
+                        <Chatroom.MessageFeed.MessageList.MessageCard.Footer className="chat-footer justify-end">
+                          <Chatroom.MessageFeed.MessageList.MessageCard.Footer.Reactions>
+                            <Chatroom.MessageFeed.MessageList.MessageCard.Footer.Reactions.ReactionList />
+                          </Chatroom.MessageFeed.MessageList.MessageCard.Footer.Reactions>
+                          <Chatroom.MessageFeed.MessageList.MessageCard.Footer.Indicators className="pr-0">
+                            <Chatroom.MessageFeed.MessageList.MessageCard.Footer.Indicators.EditedBadge />
+                            <Chatroom.MessageFeed.MessageList.MessageCard.Footer.Indicators.ReplyCount />
+                            <Chatroom.MessageFeed.MessageList.MessageCard.Header.Timestamp />
+                            <Chatroom.MessageFeed.MessageList.MessageCard.Footer.Indicators.MessageSeen />
+                          </Chatroom.MessageFeed.MessageList.MessageCard.Footer.Indicators>
+                        </Chatroom.MessageFeed.MessageList.MessageCard.Footer>
+                      </div>
+                    </Chatroom.MessageFeed.MessageList.MessageCard>
+                  </Chatroom.MessageFeed.MessageList.MessageCard.LongPressMenu>
+                )}
+              </Chatroom.MessageFeed.MessageList.Loop>
+            </Chatroom.MessageFeed.MessageList>
           </Chatroom.MessageFeed>
           <Chatroom.ChannelComposer className="w-full" />
         </Chatroom>
@@ -101,7 +96,7 @@ const ChatContainerMobile = () => {
         className="!z-40"
         isOpen={emojiPicker.isOpen}
         onClose={handleEmojiPickerClose}
-        detent="content-height">
+        detent="content">
         <Sheet.Container>
           <Sheet.Header />
           <Sheet.Content>
