5b7cff0c24ea8dbd98e595ab537d4e2a4e2533ec
hossein <marzban98@gmail.com>
2025-11-27 17:14:20 +0330

    feat: update PWA manifest and improve SEO metadata
    - Enhanced manifest.json with additional properties including categories, language, and updated icon paths.
    - Improved app metadata in _app.tsx and _document.tsx for better SEO and PWA support, including theme color and Open Graph tags.
    - Streamlined icon references and added new sizes for better compatibility across devices.

--- packages/webapp/src/pages/_app.tsx
+++ packages/webapp/src/pages/_app.tsx
@@ -25,46 +25,20 @@ const queryClient = new QueryClient()
 const Header = () => {
   return (
     <Head>
-      <meta charSet="utf-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="robots" content="noindex, nofollow" />
-      <meta name="referrer" content="no-referrer" />
+      {/* Viewport - must be in _app for proper hydration */}
       <meta
         name="viewport"
-        content="minimum-scale=1, initial-scale=1, height=device-height, maximum-scale=1.0, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
       />
-      {/* Chrome, Firefox OS and Opera  */}
-      <meta name="theme-color" content="#3367D6" />
-      <link rel="shortcut icon" href="/icons/favicon.ico" />
-      <link rel="icon" type="image/png" href="/icons/maskable_icon_x512.png" />
-      <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
-      <meta property="og:image" content="/icons/logo.svg" />
-      <meta property="og:image:alt" content="docs.plus" />
-      <link rel="mask-icon" href="/icons/maskable_icon.png" color="#FFFFFF" />
 
-      {/* Sets whether a web application runs in full-screen mode. See: https://developer.apple.com/library/safari/documentation/AppleApplications/Reference/SafariHTMLRef/Articles/MetaTags.html#//apple_ref/doc/uid/TP40008193-SW3 */}
-      <meta name="mobile-web-app-capable" content="yes" />
-      {/* Web clip icon for Android homescreen shortcuts. Available since Chrome 31+ for Android.See: https://developers.google.com/chrome/mobile/docs/installtohomescreen */}
-      <link rel="shortcut icon" sizes="192x192" href="/icons/android-chrome-192x192.png" />
+      {/* IE compatibility */}
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 
-      <link id="apple-touch-icon" rel="apple-touch-icon" href="/icons/android-chrome-512x512.png" />
-
-      {/*
-          Disables automatic detection of possible phone numbers in a webpage in Safari on iOS.
-          See: https://developer.apple.com/library/safari/documentation/AppleApplications/Reference/SafariHTMLRef/Articles/MetaTags.html#//apple_ref/doc/uid/TP40008193-SW5
-          See: https://developer.apple.com/library/safari/documentation/AppleApplications/Reference/SafariHTMLRef/Articles/MetaTags.html#//apple_ref/html/const/format-detection
-        */}
-      <meta name="format-detection" content="telephone=no" />
-      <meta name="format-detection" content="address=no" />
+      {/* SEO keywords */}
       <meta
         name="keywords"
-        content="docs.plus, real-time, collaborative, open-source, communities, knowledge sharing, Microsoft Word alternative"
+        content="docs.plus, real-time, collaborative, open-source, communities, knowledge sharing, document editor"
       />
-      <link rel="manifest" href="/manifest.json" />
-
-      <meta name="application-name" content="docs.plus" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      <meta name="apple-mobile-web-app-title" content="docs.plus" />
     </Head>
   )
 }
--- packages/webapp/src/pages/_document.tsx
+++ packages/webapp/src/pages/_document.tsx
@@ -3,7 +3,63 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" data-theme="docsyLight">
-      <Head />
+      <Head>
+        {/* Character encoding - must be first */}
+        <meta charSet="utf-8" />
+
+        {/* PWA Manifest - critical for installability */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* iOS Safari PWA - required for standalone mode */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Docs.plus" />
+
+        {/* Android Chrome PWA */}
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="application-name" content="Docs.plus" />
+
+        {/* Theme color - browser chrome, status bar */}
+        <meta name="theme-color" content="#2778ff" />
+        <meta name="msapplication-TileColor" content="#2778ff" />
+
+        {/* Favicons */}
+        <link rel="icon" type="image/x-icon" href="/icons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+
+        {/* Apple Touch Icons - for iOS home screen */}
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-touch-icon.png" />
+
+        {/* Safari Mask Icon */}
+        <link rel="mask-icon" href="/icons/maskable_icon.png" color="#2778ff" />
+
+        {/* Android Chrome shortcut icon */}
+        <link rel="shortcut icon" sizes="192x192" href="/icons/android-chrome-192x192.png" />
+
+        {/* Disable auto-detection */}
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="format-detection" content="address=no" />
+        <meta name="format-detection" content="email=no" />
+
+        {/* SEO - basic */}
+        <meta name="robots" content="noindex, nofollow" />
+        <meta name="referrer" content="no-referrer" />
+
+        {/* Open Graph - basic */}
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Docs.plus" />
+        <meta property="og:image" content="/icons/android-chrome-512x512.png" />
+        <meta property="og:image:alt" content="Docs.plus" />
+
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="/icons/android-chrome-512x512.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
