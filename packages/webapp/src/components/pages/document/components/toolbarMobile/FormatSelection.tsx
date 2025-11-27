5352bbf37cf6981c0a13f22e76bce02c3c9e2044
hossein <marzban98@gmail.com>
2025-11-27 14:39:52 +0330

    refactor: improve mobile format selection component styling and visibility logic
    - Adjusted class names for better readability and consistency.
    - Enhanced visibility logic to use 'block' and 'hidden' classes for improved rendering behavior.

--- packages/webapp/src/components/pages/document/components/toolbarMobile/FormatSelection.tsx
+++ packages/webapp/src/components/pages/document/components/toolbarMobile/FormatSelection.tsx
@@ -15,10 +15,10 @@ import ToolbarButton from '@components/TipTap/toolbar/ToolbarButton'
 const FormatSelection = ({ isVisible, editor }: { isVisible: boolean; editor: Editor }) => {
   return (
     <div
-      className={`tiptap-toolbar-mobile__format-selection absolute left-0 top-0 z-0 w-full space-y-6 rounded-t-3xl bg-base-100 px-4 py-6 transition-all ${
+      className={`tiptap-toolbar-mobile__format-selection bg-base-100 absolute top-0 left-0 z-0 w-full space-y-6 rounded-t-3xl px-4 py-6 transition-all ${
         isVisible
-          ? '-translate-y-[134px] shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1),_0_-2px_4px_-1px_rgba(0,0,0,0.06)]'
-          : 'translate-y-[0]'
+          ? 'block -translate-y-[134px] shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1),_0_-2px_4px_-1px_rgba(0,0,0,0.06)]'
+          : 'hidden translate-y-[0]'
       } `}>
       <div className="flex items-center justify-between">
         <ToolbarButton
