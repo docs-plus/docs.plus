366f09a280fc2a33dba07d55f97aa44717eaa09e
hossein <marzban98@gmail.com>
2026-01-12 19:38:56 +0330

    fix(home): remove loading state causing rapid reloads

--- packages/webapp/src/pages/index.tsx
+++ packages/webapp/src/pages/index.tsx
@@ -1,14 +1,8 @@
 import { GetServerSidePropsContext } from 'next'
-import { useAuthStore } from '@stores'
-import Loading from '@components/ui/Loading'
 import HomePage from '@components/pages/home/HomePage'
 import { getHostname } from '../utils'
 
 function Home({ hostname }: { hostname: string }) {
-  const authLoading = useAuthStore((state) => state.loading)
-
-  if (authLoading) return <Loading size="lg" />
-
   return <HomePage hostname={hostname} />
 }
 
